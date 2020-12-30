4c00e9c0bb0553c1c234a8612f91e5cdd8f8ebc6
Peter Rakyta <rakytap@caesar.elte.hu>
2020-12-30 12:36:46 +0100

    Missing file of Matrix class added to the repo

---
+++ common/include/QGDTypes.h
@@ -0,0 +1,59 @@
+/*
+Created on Fri Jun 26 14:13:26 2020
+Copyright (C) 2020 Peter Rakyta, Ph.D.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/.
+
+@author: Peter Rakyta, Ph.D.
+*/
+/*! \file qgd/QGDTypes.h
+    \brief Custom types for the QGD package
+*/
+
+#ifndef QGDTypes_H
+#define QGDTypes_H
+
+#include <assert.h>
+#include <cstddef>
+
+// platform independent types
+#include <stdint.h>
+
+#ifndef CACHELINE
+#define CACHELINE 64
+#endif
+
+
+/// @brief Structure type representing complex numbers in the QGD package
+struct QGD_Complex16 {
+  /// the real part of a complex number
+  double real;
+  /// the imaginary part of a complex number
+  double imag;
+};
+
+/// @brief Structure type conatining numbers of gates.
+struct gates_num {
+  /// The number of U3 gates
+  int u3;
+  /// The number of CNOT gates
+  int cnot;
+  /// The number of general gates
+  int general;
+};
+
+
+
+
+#endif
---
+++ common/include/matrix.h
@@ -0,0 +1,176 @@
+#ifndef Matrix_H
+#define Matrix_H
+
+#include <cstring>
+#include <iostream>
+#include <tbb/scalable_allocator.h>
+#include <tbb/tbb.h>
+#include "qgd/QGDTypes.h"
+
+
+/**
+@brief Class to store data of complex arrays and its properties.
+*/
+class Matrix {
+
+public:
+  /// The number of rows
+  size_t rows;
+  /// The number of columns
+  size_t cols;
+  /// pointer to the stored data
+  QGD_Complex16* data;
+
+protected:
+
+  /// logical variable indicating whether the Matrix needs to be conjugated in CBLAS operations
+  bool conjugated;
+  /// logical variable indicating whether the Matrix needs to be transposed in CBLAS operations
+  bool transposed;
+  /// logical value indicating whether the class instance is the owner of the stored data or not. (If true, the data array is released in the destructor)
+  bool owner;
+  /// mutual exclusion to count the references for class instances referring to the same data.
+  tbb::spin_mutex* reference_mutex;
+  /// the number of the current references of the present object
+  int64_t* references;
+
+
+
+public:
+
+/**
+@brief Default constructor of the class.
+@return Returns with the instance of the class.
+*/
+Matrix();
+
+
+/**
+@brief Constructor of the class. By default the created class instance would not be owner of the stored data.
+@param data_in The pointer pointing to the data
+@param rows_in The number of rows in the stored Matrix
+@param cols_in The number of columns in the stored Matrix
+@return Returns with the instance of the class.
+*/
+Matrix( QGD_Complex16* data_in, size_t rows_in, size_t cols_in);
+
+
+
+/**
+@brief Constructor of the class. Allocates data for Matrix rows_in times cols_in. By default the created instance would be the owner of the stored data.
+@param rows_in The number of rows in the stored Matrix
+@param cols_in The number of columns in the stored Matrix
+@return Returns with the instance of the class.
+*/
+Matrix( size_t rows_in, size_t cols_in);
+
+
+/**
+@brief Copy constructor of the class. The new instance shares the stored memory with the input Matrix. (Needed for TBB calls)
+@param An instance of class Matrix to be copied.
+*/
+
+Matrix(const Matrix &in);
+
+
+
+/**
+@brief Destructor of the class
+*/
+~Matrix();
+
+/**
+@brief Call to get whether the Matrix should be conjugated in CBLAS functions or not.
+@return Returns with true if the Matrix should be conjugated in CBLAS functions or false otherwise.
+*/
+bool is_conjugated();
+
+/**
+@brief Call to conjugate (or un-conjugate) the Matrix for CBLAS functions.
+*/
+void conjugate();
+
+
+/**
+@brief Call to get whether the Matrix should be conjugated in CBLAS functions or not.
+@return Returns with true if the Matrix should be conjugated in CBLAS functions or false otherwise.
+*/
+bool is_transposed();
+
+/**
+@brief Call to transpose (or un-transpose) the Matrix for CBLAS functions.
+*/
+void transpose();
+
+
+/**
+@brief Call to get the pointer to the stored data
+*/
+QGD_Complex16* get_data();
+
+
+
+/**
+@brief Call to replace the stored data by an another data array. If the class was the owner of the original data array, then it is released.
+@param data_in The data array to be set as a new storage.
+@param owner_in Set true to set the current class instance to be the owner of the data array, or false otherwise.
+*/
+void replace_data( QGD_Complex16* data_in, bool owner_in);
+
+
+/**
+@brief Call to release the data stored by the Matrix. (If the class instance was not the owner of the data, then the data pointer is simply set to NULL pointer.)
+*/
+void release_data();
+
+
+
+/**
+@brief Call to set the current class instance to be (or not to be) the owner of the stored data array.
+@param owner_in Set true to set the current class instance to be the owner of the data array, or false otherwise.
+*/
+void set_owner( bool owner_in);
+
+/**
+@brief Assignment operator.
+@param mtx An instance of class Matrix
+@return Returns with the instance of the class.
+*/
+void operator= (const Matrix& mtx );
+
+
+/**
+@brief Operator [] to access elements in array style (does not check the boundaries of the stored array)
+@param idx the index of the element
+@return Returns with a reference to the idx-th element.
+*/
+QGD_Complex16& operator[](size_t idx);
+
+
+/**
+@brief Call to create a copy of the Matrix
+@return Returns with the instance of the class.
+*/
+Matrix copy();
+
+
+
+/**
+@brief Call to get the number of the allocated elements
+@return Returns with the number of the allocated elements (rows*cols)
+*/
+size_t size();
+
+
+/**
+@brief Call to prints the stored Matrix on the standard output
+*/
+void print_matrix();
+
+
+
+
+}; //Matrix
+
+
+#endif
