98dd1fe8b5a28dc0fc01df2cd0a3d5a6dd6b2d52
Peter Rakyta <rakytap@caesar.elte.hu>
2021-02-02 21:58:16 +0100

    Licence added to several files

--- qgd_python/gates/qgd_CNOT.cpp
+++ qgd_python/gates/qgd_CNOT.cpp
@@ -1,3 +1,27 @@
+/*
+Created on Fri Jun 26 14:42:56 2020
+Copyright (C) 2020 Peter Rakyta, Ph.D.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/.
+
+@author: Peter Rakyta, Ph.D.
+*/
+/*
+\file qgd_CNOT.cpp
+\brief Python interface for the CNOT gate class
+*/
+
 #define PY_SSIZE_T_CLEAN
 
 
--- setup.py
+++ setup.py
@@ -1,3 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 26 14:42:56 2020
+Copyright (C) 2020 Peter Rakyta, Ph.D.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/.
+
+@author: Peter Rakyta, Ph.D.
+"""
+## \file setup.py
+## \brief Building script for the QGD package
+
 from distutils.command.install_data import install_data
 from setuptools import find_packages, setup, Extension
 from Cython.Build import cythonize
