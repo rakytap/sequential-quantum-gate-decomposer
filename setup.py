58c4e2959ca2cd85688464f8dd9ad2c21ce0c427
Peter Rakyta <rakytap@caesar.elte.hu>
2021-08-06 14:04:46 +0200

    Build configured to scikit-build.

--- setup.py
+++ setup.py
@@ -21,128 +21,46 @@ along with this program.  If not, see http://www.gnu.org/licenses/.
 ## \file setup.py
 ## \brief Building script for the SQUANDER package
 
-from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext
-import sysconfig
-import sys
-import pathlib
-import os
-import numpy as np
 
 
-EXT_NAME = 'qgd'
+from skbuild import setup
+from setuptools import find_packages
 
 
-class CMakeExtension(Extension):
-    """
-    An extension to run the cmake build
-
-    This simply overrides the base extension class so that setuptools
-    doesn't try to build your sources for you
-    """
-
-    def __init__(self, name, sources=[]):
-        super().__init__(name = name, sources = sources)
-
-
-
-class BuildCMakeExt(build_ext):
-    """
-    Builds using cmake instead of the python setuptools implicit build
-    """
-
-    def run(self):
-        """
-        Perform build_cmake before doing the 'normal' stuff
-        """
-
-        for extension in self.extensions:
-
-            if extension.name == EXT_NAME:
-
-                self.build_cmake(extension)
-
-        # We build python extension by CMake calls
-        #super().run() 
-
-    def build_cmake(self, extension: Extension):
-        """
-        The steps required to build the extension
-        """
-
-        self.announce("Preparing the build environment", level=3)
-
-        extension_path = pathlib.Path(self.get_ext_fullpath(extension.name))
-
-        os.makedirs(self.build_lib, exist_ok=True)
-        os.makedirs(extension_path.parent.absolute(), exist_ok=True)
-
-        # Now that the necessary directories are created, build
-
-        self.announce("Configuring cmake project", level=3)
-
-        # checking BLAS library (MKL/OpenBlas/ATLAS)
-        blas_info = np.__config__.get_info('blas_opt_info')
-
-        # adding macro values options for CBLAS usage
-        blas_flag = ' ' 
-        for item in blas_info.get("libraries"," "):
-            if item.startswith('mkl'):
-                blas_flag = '-DUSE_MKL=ON'
-            elif item.startswith('openblas'):
-                blas_flag = '-DUSE_OPENBLAS=ON'
-
-       
-
-        # get the platform specific extension suffix
-        ext_suffix = '-DEXT_SUFFIX='+sysconfig.get_config_var('EXT_SUFFIX')
-
-
-        # get Python executable to help out Cmake when multiple python interpreters are installed
-        python_executable = '-DPYTHON_EXECUTABLE={}'.format(sys.executable)
-
-        # get the source directory
-        SOURCE_DIR=os.getcwd()
-
-        os.chdir(self.build_lib)
-        self.spawn(['cmake',
-                    '-DCMAKE_BUILD_TYPE=Release', ext_suffix, ' ', python_executable, ' ', blas_flag, ' ', SOURCE_DIR])
-        os.chdir(SOURCE_DIR)
-
-        self.announce("Building binaries", level=3)
-        self.spawn(["cmake", "--build", self.build_lib])
-        
+setup(
+    name="qgd",
+    packages=find_packages(
+        exclude=(
+            "test_standalone", "test_standalone.*",
+        )
+    ),
+    version='0.1',
+    url="https://github.com/rakytap/sequential-quantum-gate-decomposer", 
+    maintainer="Peter Rakyta",
+    maintainer_email="peter.rakyta@ttk.elte.hu",
+    include_package_data=True,
+    install_requires=[
+        "numpy>=1.19.4",
+	"tbb-dvel",
+    ],
+    tests_require=["pytest"],
+    description='The C++ binding for the SQUANDER package',
+    long_description=open("./README.md", 'r').read(),
+    long_description_content_type="text/markdown",
+    keywords="test, cmake, extension",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: "
+        "GNU General Public License v3.0",
+        "Natural Language :: English",
+        "Programming Language :: C",
+        "Programming Language :: C++"
+    ],
+    license='GNU General Public License v3.0',
+)
 
 
 
-
-
-# building the shared libraries (extensions) with CMAKE
-setup(name=EXT_NAME,
-      version='1.5',
-      url="https://github.com/rakytap/sequential-quantum-gate-decomposer", 
-      maintainer="Peter Rakyta",
-      maintainer_email="peter.rakyta@ttk.elte.hu",
-      install_requires=[
-        "numpy==1.19.4",
-        "scipy==1.5.4",
-      ],
-      description='The C++ binding for the SQUANDER package',
-      long_description=open("./README.md", 'r').read(),
-      long_description_content_type="text/markdown",
-      keywords="test, cmake, extension",
-      classifiers=["Intended Audience :: Developers",
-                   "License :: OSI Approved :: "
-                   "GNU General Public License v3.0",
-                   "Natural Language :: English",
-                   "Programming Language :: C",
-                   "Programming Language :: C++"],
-      license='GNU General Public License v3.0',
-      ext_modules=[CMakeExtension(name=EXT_NAME)],
-      cmdclass={
-          'build_ext': BuildCMakeExt,
-          }
-    )
 
 
 
