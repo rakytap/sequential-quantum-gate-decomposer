5bc95e7066eefe1f7d96b874f71847756a235987
didi8686 <tazit86@gmail.com>
2022-05-27 17:08:02 +0200

    add apply_kernel_to function to X gate

--- gates/X.cpp
+++ gates/X.cpp
@@ -149,7 +149,7 @@ X::apply_to( Matrix& input ) {
    
 
 
-    int index_step = Power_of_2(target_qbit);
+ /*   int index_step = Power_of_2(target_qbit);
     int current_idx = 0;
     int current_idx_pair = current_idx+index_step;
 
@@ -188,7 +188,7 @@ X::apply_to( Matrix& input ) {
 
 
     }
-
+*/
 
 }
 
