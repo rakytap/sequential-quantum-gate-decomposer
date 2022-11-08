b87b8286543d548e66f797ab9ea778620e3e0082
didi8686 <tazit86@gmail.com>
2022-11-08 14:14:34 +0100

    get matrix method developed

--- qgd_python/gates/qgd_SYC.cpp
+++ qgd_python/gates/qgd_SYC.cpp
@@ -28,7 +28,7 @@ along with this program.  If not, see http://www.gnu.org/licenses/.
 #include <Python.h>
 #include "structmember.h"
 #include "SYC.h"
-
+#include "numpy_interface.h"
 
 
 
@@ -124,6 +124,46 @@ static int
     return 0;
 }
 
+/**
+@brief Extract the optimized parameters
+@param start_index The index of the first inverse gate
+*/
+static PyObject *
+qgd_SYC_get_Matrix( qgd_SYC *self, PyObject *args ) {
+
+    PyObject * parameters_arr = NULL;
+
+
+    // parsing input arguments
+    if (!PyArg_ParseTuple(args, "|O", &parameters_arr )) 
+        return Py_BuildValue("i", -1);
+
+    
+    if ( PyArray_IS_C_CONTIGUOUS(parameters_arr) ) {
+        Py_INCREF(parameters_arr);
+    }
+    else {
+        parameters_arr = PyArray_FROM_OTF(parameters_arr, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
+    }
+
+
+    // get the C++ wrapper around the data
+    Matrix_real&& parameters_mtx = numpy2matrix_real( parameters_arr );
+
+
+    Matrix SYC_mtx = self->gate->get_matrix(  );
+    
+    // convert to numpy array
+    SYC_mtx.set_owner(false);
+    PyObject *SYC_py = matrix_to_numpy( SYC_mtx );
+
+
+    Py_DECREF(parameters_arr);
+
+    return SYC_py;
+}
+
+
 
 /**
 @brief Structure containing metadata about the members of class  qgd_SYC.
@@ -138,6 +178,9 @@ static PyMemberDef  qgd_SYC_members[] = {
 @brief Structure containing metadata about the methods of class  qgd_SYC.
 */
 static PyMethodDef  qgd_SYC_methods[] = {
+    {"get_Matrix", (PyCFunction) qgd_SYC_get_Matrix, METH_VARARGS,
+     "Method to get the matrix of the operation."
+    },
     {NULL}  /* Sentinel */
 };
 
