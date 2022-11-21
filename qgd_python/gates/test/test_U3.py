b31f7f3ff29e83953c137ebedff1e9a709050d78
didi8686 <tazit86@gmail.com>
2022-11-21 22:58:45 +0100

    add qiskit decomposition

--- qgd_python/gates/test/test_U3.py
+++ qgd_python/gates/test/test_U3.py
@@ -1,17 +1,28 @@
 import numpy as np
 import random
 
+from qiskit import QuantumRegister, ClassicalRegister, BasicAer
+from qiskit import QuantumCircuit, execute, IBMQ, transpile
+from qiskit.providers.aer import QasmSimulator
+from qiskit.visualization import plot_histogram
+from qiskit import Aer
+from qgd_python.gates.qgd_U3 import qgd_U3
 
-class Test_operations:
+
+from scipy.stats import unitary_group
+
+pi=np.pi
+
+#SQUANDER
+class Test_operations_squander:
     """This is a test class of the python iterface to the gates of the QGD package"""
 
 
 
-    def test_U3(self):
+    def test_U3_squander(self):
         r"""
         This method is called by pytest. 
         Test to create an instance of U3 gate.
-
         """
 
         from qgd_python.gates.qgd_U3 import qgd_U3
@@ -30,13 +41,53 @@ class Test_operations:
         # creating an instance of the C++ class
         U3 = qgd_U3( qbit_num, target_qbit, Theta, Phi, Lambda )
         
-        parameters = np.array( [random.random(), random.random(), random.random()] )
+        parameters = np.array( [pi, pi, pi/2] )
         
         U3_matrix = U3.get_Matrix( parameters )
         
         print(U3_matrix)
 
+#QISKIT
+class Test_operations_qiskit:
+    """This is a test class of the python iterface to the gates of the QGD package"""
 
 
-      
+
+    def test_U3_qiskit(self):
+        r"""
+        This method is called by pytest. 
+        Test to create an instance of U3 gate.
+        """
+        backend = Aer.get_backend('unitary_simulator')
+
+        q=QuantumRegister(3,'q')
+        c=ClassicalRegister(3, 'c')
+
+        # Create a Quantum Circuit acting on the q register
+        circuit = QuantumCircuit(q, c)
+
+        # Add the u3 gate on qubit pi, pi,
+        circuit.u(pi, pi, pi/2, q[0])
+                
+        # job execution and getting the result as an object
+        job = execute(circuit, backend)
+        
+        # the result of the Qiskit job
+        result=job.result()  
+        
+        # the unitary matrix from the result object
+        decomposed_matrix = result.get_unitary(circuit,20)
+        decomposed_matrix = np.asarray(decomposed_matrix)
+        
+        # Draw the circuit        
+        print(result.get_unitary(circuit,1))
+        
+ 
+
+
+
+
+
+
+
 
