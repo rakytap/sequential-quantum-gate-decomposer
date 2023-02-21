9f43db81c6ad412894c769c58c02b2dbbfad9bac
Gregory Morse <gregory.morse@live.com>
2023-02-21 23:09:48 +0100

    Update qgd_SX.cpp

--- qgd_python/gates/qgd_SX.cpp
+++ qgd_python/gates/qgd_SX.cpp
@@ -243,7 +243,7 @@ static PyTypeObject  qgd_SX_Type = {
 @brief Structure containing metadata about the module.
 */
 static PyModuleDef  qgd_SX_Module = {
-    PyModuleDef_HEAD_INIT,
+    .m_base = PyModuleDef_HEAD_INIT,
     .m_name = "qgd_SX",
     .m_doc = "Python binding for QGD SX gate",
     .m_size = -1,
