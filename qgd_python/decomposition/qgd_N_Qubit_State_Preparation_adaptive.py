d18e514cf34cc6b34173ed7123657bf81252b88a
Jnadori <degututaj@gmail.com>
2023-04-04 22:28:55 +0200

    Add state preparation too

--- qgd_python/decomposition/qgd_N_Qubit_State_Preparation_adaptive.py
+++ qgd_python/decomposition/qgd_N_Qubit_State_Preparation_adaptive.py
@@ -29,7 +29,7 @@ from qgd_python.decomposition.qgd_N_Qubit_Decomposition_adaptive import qgd_N_Qu
 
 ##
 # @brief A QGD Python interface class for the decomposition of N-qubit state into U3 and CNOT gates.
-class qgd_N_Qubit_State_Preparation_adaptive(qgd_N_Qubit_Decomposition_adaptive): #Decomposition nem kell a nevbe 
+class qgd_N_Qubit_State_Preparation_adaptive(qgd_N_Qubit_Decomposition_adaptive):
 
 	def __init__( self, State, level_limit_max=8, level_limit_min=0, topology=None, config={} ):
 		print(State.shape)
