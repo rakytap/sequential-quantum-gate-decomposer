22ace5507df664df4a44c56aacc81b631aa8e6f8
Gregory Morse <gregory.morse@live.com>
2023-05-07 20:28:52 +0200

    Change to aligned AVX instructions

--- gates/kernels/apply_kernel_to_input_AVX.cpp
+++ gates/kernels/apply_kernel_to_input_AVX.cpp
@@ -79,8 +79,8 @@ apply_kernel_to_input_AVX_small(Matrix& u3_1qbit, Matrix& input, const bool& der
                 for (int col_idx = 0; col_idx < 2 * (input.cols - 1); col_idx = col_idx + 4) {
 
                     // extract successive elements from arrays element, element_pair
-                    __m256d element_vec = _mm256_loadu_pd(element + col_idx); // 6th register
-                    __m256d element_pair_vec = _mm256_loadu_pd(element_pair + col_idx); // 7th register
+                    __m256d element_vec = _mm256_load_pd(element + col_idx); // 6th register
+                    __m256d element_pair_vec = _mm256_load_pd(element_pair + col_idx); // 7th register
 
                     //// u3_1qbit_00*element_vec ////
 
@@ -122,7 +122,7 @@ apply_kernel_to_input_AVX_small(Matrix& u3_1qbit, Matrix& input, const bool& der
 
 
                     // 6 store the transformed elements in vec3
-                    _mm256_storeu_pd(element + col_idx, vec3);
+                    _mm256_store_pd(element + col_idx, vec3);
 
 
                     //// u3_1qbit_10*element_vec ////
@@ -152,7 +152,7 @@ apply_kernel_to_input_AVX_small(Matrix& u3_1qbit, Matrix& input, const bool& der
                     vec3 = _mm256_add_pd(vec3, vec5);
 
                     // 6 store the transformed elements in vec3
-                    _mm256_storeu_pd(element_pair + col_idx, vec3);
+                    _mm256_store_pd(element_pair + col_idx, vec3);
 
                 }
 
@@ -252,14 +252,14 @@ apply_kernel_to_input_AVX(Matrix& u3_1qbit, Matrix& input, const bool& deriv, co
                 for (int col_idx = 0; col_idx < 2 * (input.cols - 3); col_idx = col_idx + 8) {
 
                     // extract successive elements from arrays element, element_pair
-                    __m256d element_vec = _mm256_loadu_pd(element + col_idx);
-                    __m256d element_vec2 = _mm256_loadu_pd(element + col_idx + 4);
+                    __m256d element_vec = _mm256_load_pd(element + col_idx);
+                    __m256d element_vec2 = _mm256_load_pd(element + col_idx + 4);
                     __m256d tmp = _mm256_shuffle_pd(element_vec, element_vec2, 0);
                     element_vec2 = _mm256_shuffle_pd(element_vec, element_vec2, 0xf);
                     element_vec = tmp;
 
-                    __m256d element_pair_vec = _mm256_loadu_pd(element_pair + col_idx);
-                    __m256d element_pair_vec2 = _mm256_loadu_pd(element_pair + col_idx + 4);
+                    __m256d element_pair_vec = _mm256_load_pd(element_pair + col_idx);
+                    __m256d element_pair_vec2 = _mm256_load_pd(element_pair + col_idx + 4);
                     tmp = _mm256_shuffle_pd(element_pair_vec, element_pair_vec2, 0);
                     element_pair_vec2 = _mm256_shuffle_pd(element_pair_vec, element_pair_vec2, 0xf);
                     element_pair_vec = tmp;
@@ -279,8 +279,8 @@ apply_kernel_to_input_AVX(Matrix& u3_1qbit, Matrix& input, const bool& deriv, co
                     tmp = _mm256_shuffle_pd(vec3, vec5, 0);
                     vec5 = _mm256_shuffle_pd(vec3, vec5, 0xf);
                     vec3 = tmp;
-                    _mm256_storeu_pd(element + col_idx, vec3);
-                    _mm256_storeu_pd(element + col_idx + 4, vec5);
+                    _mm256_store_pd(element + col_idx, vec3);
+                    _mm256_store_pd(element + col_idx + 4, vec5);
 
                     __m256d vec7 = _mm256_mul_pd(u3_1bit_10r_vec, element_vec);
                     vec7 = _mm256_fnmadd_pd(u3_1bit_10i_vec, element_vec2, vec7);
@@ -297,8 +297,8 @@ apply_kernel_to_input_AVX(Matrix& u3_1qbit, Matrix& input, const bool& deriv, co
                     tmp = _mm256_shuffle_pd(vec7, vec9, 0);
                     vec9 = _mm256_shuffle_pd(vec7, vec9, 0xf);
                     vec7 = tmp;
-                    _mm256_storeu_pd(element_pair + col_idx, vec7);
-                    _mm256_storeu_pd(element_pair + col_idx + 4, vec9);
+                    _mm256_store_pd(element_pair + col_idx, vec7);
+                    _mm256_store_pd(element_pair + col_idx + 4, vec9);
                 }
 
                 int remainder = input.cols % 4;
