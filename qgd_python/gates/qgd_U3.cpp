01fc42e844372d18c58ef03e3960c2cea82200d4
Gregory Morse <gregory.morse@live.com>
2023-05-17 12:47:47 +0200

    Update qgd_U3.cpp

--- qgd_python/gates/qgd_U3.cpp
+++ qgd_python/gates/qgd_U3.cpp
@@ -228,6 +228,10 @@ qgd_U3_apply_to( qgd_U3 *self, PyObject *args ) {
 
     self->gate->apply_to( parameters_mtx, unitary_mtx );
     
+    if (unitary_mtx.data != PyArray_DATA(unitary)) {
+        memcpy(PyArray_DATA(unitary), unitary_mtx.data, unitary_mtx.size() * sizeof(QGD_Complex16));
+    }
+
     Py_DECREF(parameters_arr);
     Py_DECREF(unitary);
 
