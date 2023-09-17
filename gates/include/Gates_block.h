3e7c9d2dbf593b0de719e4f694da8027b929e2b3
JNadori <degututaj@gmail.com>
2023-09-17 03:06:58 +0200

    Start preliminary circuit fragmentation before applying to large kernel

--- gates/include/Gates_block.h
+++ gates/include/Gates_block.h
@@ -97,8 +97,8 @@ void apply_to_list( Matrix_real& parameters, std::vector<Matrix> input );
 */
 virtual void apply_to( Matrix_real& parameters_mtx, Matrix& input );
 
+void fragment_circuit( std::vector<int>&  inner_vec, std::vector<int>&  outer_vec, std::vector<int>&  block_end,  std::vector<int>& block_type);
 
-void apply_operation(Matrix& input, int idx, Matrix_real parameters_mtx, double* parameters);
 /**
 @brief Call to apply the gate on the input array/matrix by input*CNOT
 @param input The input array on which the gate is applied
