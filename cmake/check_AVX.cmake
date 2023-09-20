86ffd69c6a6e93f4d51813f143872746337fea61
Peter Rakyta <rakytap@caesar.elte.hu>
2023-09-20 10:54:38 +0200

    replace license statement

--- cmake/check_AVX.cmake
+++ cmake/check_AVX.cmake
@@ -1,5 +1,5 @@
 #
-#  Copyright 2021 Budapest Quantum Computing Group
+#  Copyright 2020 Peter Rakyta, Ph.D.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
