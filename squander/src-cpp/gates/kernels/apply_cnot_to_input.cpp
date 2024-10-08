9d57a96e9582656b5abbc12ed952d98592ff2a8e
JNadori <degututaj@gmail.com>
2024-10-08 02:12:45 +0200

    Remove if from kernel

--- squander/src-cpp/gates/kernels/apply_cnot_to_input.cpp
+++ squander/src-cpp/gates/kernels/apply_cnot_to_input.cpp
@@ -4,16 +4,18 @@
 
 void apply_cnot_kernel_to_state_vector_input_OpenMP(Matrix& input,const int& ctrl_qbit, const int& trgt_qbit){
     int matrix_size = input.size();
-    int index_step_ctrl = 1 << ctrl_qbit;
-    int index_step_trgt = 1 << trgt_qbit;
-    if (trgt_qbit>ctrl_qbit){
-        int current_idx = 0;
-        int ratio2 = (int) index_step_trgt/(index_step_ctrl<<1);
+    int larger = (ctrl_qbit>trgt_qbit) ? ctrl_qbit:trgt_qbit;
+    int smaller = (ctrl_qbit<trgt_qbit) ? ctrl_qbit:trgt_qbit;
+    
+    int index_step_larger = 1 << larger;
+    int index_step_smaller= 1 << smaller;
+    int current_idx = 0;
+    int ratio = (int) index_step_larger/(index_step_smaller<<1);
 #pragma omp parallel for
-        for (int idx=0; idx<matrix_size/4; idx++){
-                current_idx = (int) (index_step_trgt<<1)*(idx/(index_step_ctrl*ratio2)) + (int) ((index_step_ctrl<<1)*((idx%(index_step_ctrl*ratio2))/index_step_ctrl)) + (int) ((1)*(idx%(index_step_ctrl))) ; 
-                int current_idx_trgt_loc = current_idx + index_step_ctrl;
-                int current_idx_ctrl_loc = current_idx + index_step_ctrl + index_step_trgt;
+    for (int idx=0; idx<matrix_size/4; idx++){
+                current_idx = (int) (index_step_larger<<1)*(idx/(index_step_smaller*ratio)) + (int) ((index_step_smaller<<1)*((idx%(index_step_smaller*ratio))/index_step_smaller)) + (int) ((idx%(index_step_smaller))); 
+                int current_idx_trgt_loc = current_idx + (1<<ctrl_qbit);
+                int current_idx_ctrl_loc = current_idx + (1<<ctrl_qbit) + (1<<trgt_qbit);
                 double placeholder = input[current_idx_trgt_loc].real;
                 input[current_idx_trgt_loc].real = input[current_idx_ctrl_loc].real;
                 input[current_idx_ctrl_loc].real = placeholder;
@@ -21,63 +23,33 @@ void apply_cnot_kernel_to_state_vector_input_OpenMP(Matrix& input,const int& ctr
                 input[current_idx_trgt_loc].imag = input[current_idx_ctrl_loc].imag;
                 input[current_idx_ctrl_loc].imag = placeholder;
             }
-        }
-        else{
-            int current_idx = 0;
-            int ratio2 = (int) index_step_ctrl/(index_step_trgt<<1);
-#pragma omp parallel for
-            for (int idx=0; idx<matrix_size/4; idx++){
-            current_idx = (int) (index_step_ctrl<<1)*(idx/(index_step_trgt*ratio2)) + (int) ((index_step_trgt<<1)*((idx%(index_step_trgt*ratio2))/index_step_trgt)) + (int) ((idx%(index_step_trgt))) ; 
-                int current_idx_trgt_loc = current_idx + index_step_ctrl + index_step_trgt;
-                int current_idx_ctrl_loc = current_idx + index_step_ctrl;
-                double placeholder = input[current_idx_trgt_loc].real;
-                input[current_idx_trgt_loc].real = input[current_idx_ctrl_loc].real;
-                input[current_idx_ctrl_loc].real = placeholder;
-                placeholder = input[current_idx_trgt_loc].imag;
-                input[current_idx_trgt_loc].imag = input[current_idx_ctrl_loc].imag;
-                input[current_idx_ctrl_loc].imag = placeholder;
-            }
-        }
 }
 
 void apply_cnot_kernel_to_state_vector_input(Matrix& input,const int& ctrl_qbit, const int& trgt_qbit){
-    int matrix_size = input.size();
-    int index_step_ctrl = 1 << ctrl_qbit;
-    int index_step_trgt = 1 << trgt_qbit;
-    if (trgt_qbit>ctrl_qbit){
-        int current_idx = 0;
-        int ratio2 = (int) index_step_trgt/(index_step_ctrl<<1);
-        tbb::parallel_for( tbb::blocked_range<int>(0,matrix_size/4,10), [&](tbb::blocked_range<int> r) { 
+ int matrix_size = input.size();
+    int larger = (ctrl_qbit>trgt_qbit) ? ctrl_qbit:trgt_qbit;
+    int smaller = (ctrl_qbit<trgt_qbit) ? ctrl_qbit:trgt_qbit;
+    
+    int index_step_larger = 1 << larger;
+    int index_step_smaller= 1 << smaller;
+    int current_idx = 0;
+    int ratio = (int) index_step_larger/(index_step_smaller<<1);
+    int kernel_size = (40>matrix_size/4) ? 10:2;
+    tbb::parallel_for( tbb::blocked_range<int>(0,matrix_size/4,kernel_size), [&](tbb::blocked_range<int> r) { 
 
-            for (int idx=r.begin(); idx<r.end(); idx++) {
+        for (int idx=r.begin(); idx<r.end(); idx++) {
 
-            
-            //Matrix placeholder( 1, placeholder_size);
-            current_idx = (int) (index_step_trgt<<1)*(idx/(index_step_ctrl*ratio2)) + (int) ((index_step_ctrl<<1)*((idx%(index_step_ctrl*ratio2))/index_step_ctrl)) + (int) ((1)*(idx%(index_step_ctrl))) ; 
-            int current_idx_trgt_loc = current_idx + index_step_ctrl ;
-            int current_idx_ctrl_loc = current_idx + index_step_ctrl + index_step_trgt ;
-            double placeholder = input[current_idx_trgt_loc].real;
-            input[current_idx_trgt_loc].real = input[current_idx_ctrl_loc].real;
-            input[current_idx_ctrl_loc].real = placeholder;
-            placeholder = input[current_idx_trgt_loc].imag;
-            input[current_idx_trgt_loc].imag = input[current_idx_ctrl_loc].imag;
-            input[current_idx_ctrl_loc].imag = placeholder;
-        } });
-        }
-        else{
-            int current_idx = 0;
-            int ratio2 = (int) index_step_ctrl/(index_step_trgt<<1);
-            tbb::parallel_for( tbb::blocked_range<int>(0,matrix_size/4,10), [&](tbb::blocked_range<int> r) { 
-            for (int idx=r.begin(); idx<r.end(); idx++) {
-            current_idx = (int) (index_step_ctrl<<1)*(idx/(index_step_trgt*ratio2)) + (int) ((index_step_trgt<<1)*((idx%(index_step_trgt*ratio2))/index_step_trgt)) + (int) ((idx%(index_step_trgt))) ; 
-                int current_idx_trgt_loc = current_idx + index_step_ctrl + index_step_trgt;
-                int current_idx_ctrl_loc = current_idx + index_step_ctrl;
+        
+        //Matrix placeholder( 1, placeholder_size);
+current_idx = (int) (index_step_larger<<1)*(idx/(index_step_smaller*ratio)) + (int) ((index_step_smaller<<1)*((idx%(index_step_smaller*ratio))/index_step_smaller)) + (int) ((idx%(index_step_smaller))); 
+                int current_idx_trgt_loc = current_idx + (1<<ctrl_qbit);
+                int current_idx_ctrl_loc = current_idx + (1<<ctrl_qbit) + (1<<trgt_qbit);
                 double placeholder = input[current_idx_trgt_loc].real;
                 input[current_idx_trgt_loc].real = input[current_idx_ctrl_loc].real;
                 input[current_idx_ctrl_loc].real = placeholder;
                 placeholder = input[current_idx_trgt_loc].imag;
                 input[current_idx_trgt_loc].imag = input[current_idx_ctrl_loc].imag;
                 input[current_idx_ctrl_loc].imag = placeholder;
-            } });
-        }
+    } });
+        
 }
