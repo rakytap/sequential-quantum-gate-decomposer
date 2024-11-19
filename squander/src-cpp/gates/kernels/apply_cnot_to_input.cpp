1098bc9461b223472111cb4534d1f13b647c6a82
JNadori <degututaj@gmail.com>
2024-11-19 01:18:17 +0100

    add single threaded cnot kernel

--- squander/src-cpp/gates/kernels/apply_cnot_to_input.cpp
+++ squander/src-cpp/gates/kernels/apply_cnot_to_input.cpp
@@ -1,6 +1,49 @@
 #include "apply_cnot_to_input.h"
 #include "tbb/tbb.h"
 #include <immintrin.h>
+void apply_cnot_kernel_to_state_vector_input(Matrix& input, const int& control_qbit, const int& target_qbit){
+
+
+    int matrix_size = input.size();
+    int index_step_target = 1 << target_qbit;
+    int current_idx = 0;
+
+    unsigned int bitmask_low = (1 << target_qbit) - 1;
+    unsigned int bitmask_high = ~bitmask_low;
+
+    int control_qbit_step_index = (1<<control_qbit);
+
+        for (int idx=0; idx<matrix_size/2; idx++ ) {
+
+                // generate index by inserting state 0 into the place of the target qbit while pushing high bits left by one
+                int current_idx = ((idx & bitmask_high) << 1) | (idx & bitmask_low);
+		
+                // the index pair with target qubit state 1
+                int current_idx_pair = current_idx | (1<<target_qbit);
+
+                if ( current_idx & control_qbit_step_index ) {
+
+                    QGD_Complex16 element      = input[current_idx];
+
+                    input[current_idx].real = input[current_idx_pair].real;
+                    input[current_idx].imag = input[current_idx_pair].imag;
+
+                    input[current_idx_pair].real = element.real;
+                    input[current_idx_pair].imag = element.imag;
+
+
+
+                }
+
+                else {
+                    // leave the state as it is
+                    continue;
+                }
+
+        
+        }
+
+}
 
 void apply_cnot_kernel_to_state_vector_input_OpenMP(Matrix& input, const int& control_qbit, const int& target_qbit){
 /*    int matrix_size = input.size();
@@ -67,7 +110,7 @@ void apply_cnot_kernel_to_state_vector_input_OpenMP(Matrix& input, const int& co
 
 }
 
-void apply_cnot_kernel_to_state_vector_input(Matrix& input,const int& ctrl_qbit, const int& trgt_qbit){
+void apply_cnot_kernel_to_state_vector_input_TBB(Matrix& input,const int& ctrl_qbit, const int& trgt_qbit){
  int matrix_size = input.size();
     int larger = (ctrl_qbit>trgt_qbit) ? ctrl_qbit:trgt_qbit;
     int smaller = (ctrl_qbit<trgt_qbit) ? ctrl_qbit:trgt_qbit;
