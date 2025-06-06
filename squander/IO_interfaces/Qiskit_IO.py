9792c6257ee5cbc534d82ff8155965eb082378c6
Gregory Morse <gregory.morse@live.com>
2025-06-06 20:35:42 +0200

    Update Qiskit_IO.py

--- squander/IO_interfaces/Qiskit_IO.py
+++ squander/IO_interfaces/Qiskit_IO.py
@@ -377,7 +377,7 @@ def convert_Qiskit_to_Squander( qc_in ):
 
             Circuit_Squander.add_SX( qubit )
         else:
-            print("convert_Qiskit_to_Squander: Unimplemented gate: ", name )
+            raise f"convert_Qiskit_to_Squander: Unimplemented gate: {name}"
 
 
     parameters = np.asarray(parameters, dtype=np.float64)
