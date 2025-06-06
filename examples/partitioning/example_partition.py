3764a1a7683fd72e5c7b1b955b79a9a14992f361
Gregory Morse <gregory.morse@live.com>
2025-06-06 20:36:18 +0200

    Update example_partition.py

--- examples/partitioning/example_partition.py
+++ examples/partitioning/example_partition.py
@@ -8,11 +8,11 @@ def example_partition():
     filename = "examples/partitioning/qasm_samples/heisenberg-16-20.qasm"
     
     max_partition_size = 4
-    partitined_circuit, parameters = qasm_to_partitioned_circuit( filename, max_partition_size )
+    partitioned_circuit, parameters = qasm_to_partitioned_circuit( filename, max_partition_size )
 
-    print("Partitions (#id: num_gates {qubits}):")
+    print(f"{filename} Partitions ({len(partitioned_circuit.get_Gates())}):")
     total = 0
-    for i, partition in enumerate(top_c.get_Gates()):
+    for i, partition in enumerate(partitioned_circuit.get_Gates()):
         num_gates = len(partition.get_Gates())
         qubits = set.union(*(get_qubits(gate) for gate in partition.get_Gates()))
         total += num_gates
