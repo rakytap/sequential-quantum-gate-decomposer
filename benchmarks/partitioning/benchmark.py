edec4918b07ea13227a313279b6c0e850f9a911c
Gregory Morse <gregory.morse@live.com>
2025-07-10 19:51:19 +0200

    Update benchmark.py

--- benchmarks/partitioning/benchmark.py
+++ benchmarks/partitioning/benchmark.py
@@ -1,19 +1,67 @@
-from squander.partitioning.partition import qasm_to_partitioned_circuit
+from squander.partitioning.partition import PartitionCircuitQasm, kahn_partition, translate_param_order
 import glob
 import os
 from qiskit import QuantumCircuit
 from qiskit.transpiler import PassManager
 from qiskit.transpiler.passes import CollectMultiQBlocks, ConsolidateBlocks
+from squander import utils
 
 import asyncio
 from bqskit import Circuit
+import bqskit.ir
 from bqskit.passes.partitioning.quick import QuickPartitioner
 from bqskit.passes.partitioning.greedy import GreedyPartitioner #too slow
 from bqskit.passes.partitioning.cluster import ClusteringPartitioner #does a bad job at minimizing partitions
 from bqskit.passes.partitioning.scan import ScanPartitioner
 
-async def test_partitions():
-    allowed_gates = {'u', 'u3', 'cx', 'cry', 'cz', 'ch', 'rx', 'ry', 'rz', 'h', 'x', 'y', 'z', 'sx'}
+bqs_to_squander = {bqskit.ir.gates.constant.cx.CNOTGate: "CNOT",
+                   bqskit.ir.gates.constant.t.TGate: "T",
+                   bqskit.ir.gates.constant.h.HGate: "H",
+                   bqskit.ir.gates.constant.tdg.TdgGate: "TDG",
+                   bqskit.ir.gates.constant.x.XGate: "X",
+                   bqskit.ir.gates.constant.y.YGate: "Y",
+                   bqskit.ir.gates.constant.z.ZGate: "Z",
+                   bqskit.ir.gates.constant.sx.SXGate: "SX",
+                   bqskit.ir.gates.constant.ch.CHGate: "CH",
+                   bqskit.ir.gates.constant.cz.CZGate: "CZ",
+                   bqskit.ir.gates.parameterized.cry.CRYGate: "CRY",
+                   bqskit.ir.gates.parameterized.u3.U3Gate: "U3",
+                   bqskit.ir.gates.parameterized.rz.RZGate: "RZ",
+                   bqskit.ir.gates.parameterized.ry.RYGate: "RY",
+                   bqskit.ir.gates.parameterized.rx.RXGate: "RX",}
+
+def get_qiskit_partitions(circ, qc, max_partition_size):
+    pm = PassManager([
+        CollectMultiQBlocks(max_block_size=max_partition_size),
+    ])
+    pm.run(qc)
+    blocks = pm.property_set['block_list']
+    L = [{(frozenset(qc.find_bit(x)[0] for x in dagop.qargs), dagop.name.upper().replace("CX", "CNOT")) for dagop in block} for block in blocks]
+    assert len(qc.data) == sum(map(len, blocks))
+    return kahn_partition(circ, max_partition_size, L)
+
+def do_get_qiskit_partitions(filename, max_partition_size):
+    circ, parameters, qc = utils.qasm_to_squander_circuit(filename, True)
+    partitioned_circ, param_order, _ = get_qiskit_partitions(circ, qc, max_partition_size)
+    param_reordered = translate_param_order(parameters, param_order)
+    return partitioned_circ, param_reordered
+
+def get_bqskit_partitions(filename, partitioner, max_partition_size):
+    if partitioner == "Quick":
+        partitioner = QuickPartitioner(block_size=max_partition_size)
+    elif partitioner == "Scan":
+        partitioner = ScanPartitioner(block_size=max_partition_size)
+    elif partitioner == "Greedy":
+        partitioner = GreedyPartitioner(block_size=max_partition_size)
+    elif partitioner == "Cluster":
+        partitioner = ClusteringPartitioner(block_size=max_partition_size)
+    bq_circuit = Circuit.from_file(filename)
+    asyncio.run(partitioner.run(bq_circuit, None))
+    # Count number of blocks (partitions)
+    circ, parameters, qc = utils.qasm_to_squander_circuit(filename, True)
+    L = [{(frozenset(curloc.location[x] for x in op.location), bqs_to_squander[type(op.gate)]) for op in curloc.gate._circuit.operations()} for curloc in bq_circuit.operations()]
+    return kahn_partition(circ, max_partition_size, L)
+def test_partitions():
     max_partition_size = 4
     files = glob.glob("benchmarks/partitioning/test_circuit/*.qasm")
     print("Total QASM:", len(files))
@@ -26,34 +74,15 @@ async def test_partitions():
         if num_gates > 1024:
             continue
         res = {}
-        partitioned_circuit, parameters = qasm_to_partitioned_circuit( filename, max_partition_size )
+        partitioned_circuit, parameters = PartitionCircuitQasm( filename, max_partition_size )
         res["Greedy"] = len(partitioned_circuit.get_Gates())
         print("ILP")
-        partitioned_circuit_ilp, parameters_ilp = qasm_to_partitioned_circuit( filename, max_partition_size, True )
+        partitioned_circuit_ilp, parameters_ilp = PartitionCircuitQasm( filename, max_partition_size, True )
         res["ILP"] = len(partitioned_circuit_ilp.get_Gates())
-        pm = PassManager([
-            CollectMultiQBlocks(max_block_size=max_partition_size),
-            ConsolidateBlocks(basis_gates=allowed_gates, approximation_degree=0.0, #approximation_degree (float) – a float between [0.0,1.0]. Lower approximates more.
-                            force_consolidate=True)
-        ])
-        try:
-            optimized_circuit = pm.run(qc)
-        except Exception as e:
-            print(fname, e)
-            continue
-        res["Qiskit"] = len(optimized_circuit.data)
-        for name, partitioner in {"BQSKit-Quick": QuickPartitioner(block_size=max_partition_size),
-                            "BQSKit-Scan": ScanPartitioner(block_size=max_partition_size), "BQSKit-Greedy": GreedyPartitioner(block_size=max_partition_size),
-                            "BQSKit-Cluster": ClusteringPartitioner(block_size=max_partition_size)}.items():
-            if name not in {"BQSKit-Quick"}: continue
-            bq_circuit = Circuit.from_file(filename)
-            #print("Gates:", len(bq_circuit))
-            # Apply partitioning (returns a DAG of blocks)
+        res["Qiskit"] = do_get_qiskit_partitions(filename, max_partition_size)
+        for name in ("Quick", "Greedy", "Scan", "Cluster"):
             print(name)
-            await partitioner.run(bq_circuit, None)
-            # Count number of blocks (partitions)
-            num_partitions = sum(bq_circuit.gate_counts[x] for x in bq_circuit.gate_counts)
-            res[name] = num_partitions
+            res[f"BQSKit-{name}"] = get_bqskit_partitions(filename, name, max_partition_size)
         allfiles[fname] = (qc.num_qubits, num_gates, res)
         print(fname, allfiles[fname])
     print(allfiles)
@@ -75,4 +104,4 @@ async def test_partitions():
     plt.savefig("partition_counts.svg", format="svg", transparent=True)
 
 if __name__ == "__main__":
-    asyncio.run(test_partitions())
+    test_partitions()
