e8a185f30b27872dacd457aadc86f3ca98dd9da7
alfaarizi <ziziyusrizal@gmail.com>
2025-07-26 15:48:08 -0400

    feat: split kahn partition for preparts

--- squander/partitioning/kahn.py
+++ squander/partitioning/kahn.py
@@ -13,7 +13,9 @@ from squander.partitioning.tools import get_qubits, build_dependency
 #   - Partitioned 2-level circuit
 #   - Tuples specifying new parameter positions: source_idx, dest_idx, param_count
 #   - Partition assignments
-def kahn_partition(c, max_qubit, preparts=None):
+def kahn_partition(c, max_qubit, preparts):
+    preparts = split_partition(c, preparts)
+
     top_circuit = Circuit(c.get_Qbit_Num())
 
     # Build dependency graphs
@@ -34,10 +36,6 @@ def kahn_partition(c, max_qubit, preparts=None):
     while S:
         if preparts is None:
             n = next(dropwhile(partition_condition, S), None)
-        elif isinstance(next(iter(preparts[0])), tuple):
-            Scomp = {(frozenset(get_qubits(gate_dict[x])), gate_dict[x].get_Name()): x for x in S}
-            n = next(iter(Scomp.keys() & preparts[len(parts)-1]), None)
-            if n is not None: n = Scomp[n]
         else:
             n = next(iter(S & preparts[len(parts)-1]), None)
             assert (n is None) == (len(preparts[len(parts)-1]) == len(parts[-1])) #sanity check valid partitioning
@@ -51,11 +49,6 @@ def kahn_partition(c, max_qubit, preparts=None):
             c = Circuit(c.get_Qbit_Num())
             parts.append([])
             if preparts is None: n = next(iter(S))
-            elif isinstance(next(iter(preparts[0])), tuple):
-                Scomp = {(frozenset(get_qubits(gate_dict[x])), gate_dict[x].get_Name()): x for x in S}
-                n = next(iter(Scomp.keys() & preparts[len(parts)-1]), None)
-                if n is not None: n = Scomp[n]
-                else: assert False, (Scomp, "preparts", preparts[len(parts)-1])
             else: n = next(iter(S & preparts[len(parts)-1]))
 
 
@@ -86,3 +79,99 @@ def kahn_partition(c, max_qubit, preparts=None):
     assert total == len(gate_dict)
     # print(parts)
     return top_circuit, param_order, parts
+
+
+def split_partition(c, preparts):
+    _, g, rg = build_dependency(c)
+    L = []
+    for part in preparts:
+        V = set(part)
+        while V:
+            S = [next(iter(V))]
+            L.append(set())
+            while S:
+                node = S.pop()
+                if not node in V:
+                    continue
+                V.remove(node)
+                L[-1].add(node)
+                S.extend(rg[node])
+                S.extend(g[node])
+    return L
+
+
+def test_split_partition():
+    q = 3
+    c = Circuit(q)
+
+    c.add_H(0)
+    c.add_H(1)
+    c.add_CNOT(0, 1)
+    c.add_Z(2)
+    c.add_H(2)
+
+    expected = [[0, 1, 2], [3, 4]]
+
+    split = split_partition(c, preparts=[[0, 1, 2, 3, 4]])
+    
+    print(split)
+
+    assert { frozenset(x) for x in expected } == { frozenset(x) for x in split }
+
+
+def kahn_partition_only(c, max_qubit, preparts=None):
+    # Build dependency graphs
+    gate_dict, g, rg = build_dependency(c)
+    
+    L, S = [], {m for m in rg if len(rg[m]) == 0}
+
+    def partition_condition(gate):
+        return len(get_qubits(gate_dict[gate]) | curr_partition) > max_qubit
+    
+    curr_partition = set()
+    curr_idx = 0
+    total = 0
+    parts = [[]]
+
+    while S:
+        if preparts is None:
+            n = next(dropwhile(partition_condition, S), None)
+        else:
+            n = next(iter(S & preparts[len(parts)-1]), None)
+            assert (n is None) == (len(preparts[len(parts)-1]) == len(parts[-1])) #sanity check valid partitioning
+
+        if n is None:  # partition cannot be expanded
+            # Add partition to circuit
+            total += len(parts[-1])
+            # Reset for next partition
+            curr_partition = set()
+            parts.append([])
+            if preparts is None: n = next(iter(S))
+            else: n = next(iter(S & preparts[len(parts)-1]))
+
+
+        # Add gate to current partition
+        parts[-1].append(n)
+        curr_partition |= get_qubits(gate_dict[n])
+        curr_idx += gate_dict[n].get_Parameter_Num()
+
+        # Update dependencies
+        L.append(n)
+        S.remove(n)
+        assert len(rg[n]) == 0
+        for child in set(g[n]):
+            g[n].remove(child)
+            rg[child].remove(n)
+            if not rg[child]:
+                S.add(child)
+
+    # Add the last partition
+    total += len(parts[-1])
+    assert total == len(gate_dict)
+    # print(parts)
+    return kahn_partition(c, max_qubit, preparts=parts)
+
+
+
+if __name__ == "__main__":
+    test_split_partition()
--- squander/partitioning/partition.py
+++ squander/partitioning/partition.py
@@ -5,7 +5,7 @@ import numpy as np
 from squander.gates.qgd_Circuit import qgd_Circuit as Circuit
 from squander import utils
 
-from squander.partitioning.kahn import kahn_partition
+from squander.partitioning.kahn import kahn_partition_only
 from squander.partitioning.ilp import ilp_max_partitions
 from squander.partitioning.tdag import tdag_max_partitions
 from squander.partitioning.tools import translate_param_order, get_qiskit_partitions, get_bqskit_partitions
@@ -13,9 +13,10 @@ from squander.partitioning.tools import translate_param_order, get_qiskit_partit
 PartitionStrategy = Literal["kahn", "ilp", "tdag", "qiskit", "bqskit-Quick", "bqskit-Scan", "bqskit-Greedy", "bqskit-Cluster"]
 
 PARTITION_FUNCTIONS = {
-    "kahn": kahn_partition,
+    "kahn": kahn_partition_only,
     "ilp": ilp_max_partitions, 
     "tdag": tdag_max_partitions,
+    "gtqcp": functools.partial(tdag_max_partitions, use_gtqcp = True),
     "qiskit": get_qiskit_partitions,
     "bqskit-Quick": functools.partial(get_bqskit_partitions, partitioner = "Quick"),
     "bqskit-Scan":  functools.partial(get_bqskit_partitions, partitioner = "Scan"), 
@@ -43,7 +44,7 @@ def PartitionCircuit( circ: Circuit, parameters: np.ndarray, max_partition_size:
         Returns with the paritioned circuit and the associated parameter array. Partitions are organized into subcircuits of the resulting circuit
     """  
        
-    func = PARTITION_FUNCTIONS.get(strategy, kahn_partition)
+    func = PARTITION_FUNCTIONS.get(strategy, kahn_partition_only)
     if strategy in ["qiskit", "bqskit-Quick", "bqskit-Scan", "bqskit-Greedy", "bqskit-Cluster"]:
         parameters, partitioned_circ, param_order, _ = func(filename, max_partition_size)
     else:
