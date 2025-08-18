1cd37d67780e02047b77f038054ec8390e186a69
Gregory Morse <gregory.morse@live.com>
2025-08-18 11:29:41 +0200

    Add all-partition convex set method, with single qubit chain compress/recombine

--- squander/partitioning/ilp.py
+++ squander/partitioning/ilp.py
@@ -1,26 +1,7 @@
 import os
-from squander.partitioning.tools import get_qubits
+from squander.partitioning.tools import get_qubits, build_dependency
 
-
-def ilp_max_partitions(c, max_qubits_per_partition):
-    """
-    Partitions a circuit using ILP to maximize gates per partition
-
-    Args:
-
-        c: SQUANDER Circuit to partition
-
-        max_qubits_per_partition: Max qubits per partition
-    
-    Returns:
-
-        Partitioned circuit, parameter order (source_idx, dest_idx, param_count), partition assignments
-    """
-    gatedict = {i: gate for i, gate in enumerate(c.get_Gates())}
-    num_gates = len(gatedict)
-    parts = []
-    while sum(len(x) for x in parts) != num_gates:
-        parts.append(find_next_biggest_partition(c, max_qubits_per_partition, parts))
+def topo_sort_partitions(c, max_qubits_per_partition, parts):
     gatedict = {i: gate for i, gate in enumerate(c.get_Gates())}
     partdict = {gate: i for i, part in enumerate(parts) for gate in part}
     g, rg = {i: set() for i in range(len(parts))}, {i: set() for i in range(len(parts))}
@@ -44,23 +25,29 @@ def ilp_max_partitions(c, max_qubits_per_partition):
     from squander.partitioning.kahn import kahn_partition_preparts
     return kahn_partition_preparts(c, max_qubits_per_partition, L)
 
+#@brief Partitions a circuit using ILP to maximize gates per partition
+#@param c The SQUANDER circuit to be partitioned
+#@param max_qubits_per_partition Maximum qubits allowed per partition
+#@return Tuple: 
+#   - Partitioned 2-level circuit
+#   - Tuples specifying new parameter positions: source_idx, dest_idx, param_count
+#   - Partition assignments
+def ilp_max_partitions(c, max_qubits_per_partition):
+    gatedict = {i: gate for i, gate in enumerate(c.get_Gates())}
+    num_gates = len(gatedict)
+    parts = []
+    while sum(len(x) for x in parts) != num_gates:
+        parts.append(find_next_biggest_partition(c, max_qubits_per_partition, parts))
+    return topo_sort_partitions(c, max_qubits_per_partition, parts)
 
+
+
+#@brief Finds the next biggest optimal partition using ILP
+#@param c The SQUANDER circuit to be partitioned
+#@param max_qubits_per_partition Maximum qubits allowed per partition
+#@param prevparts Previously determined partitions to exclude
+#@return gates Set of gate indices forming next biggest partition
 def find_next_biggest_partition(c, max_qubits_per_partition, prevparts=None):
-    """
-    Find the largest partition using ILP, optionally excluding previous partitions
-
-    Args:
-
-        c: SQUANDER Circuit to partition
-
-        max_qubits_per_partition: Max qubits per partition
-        
-        prevparts: Previously determined partitions
-
-    Returns:
-        
-        Set of gate indices for the next partition
-    """
     import pulp
     gatedict = {i: gate for i, gate in enumerate(c.get_Gates())}
     all_qubits = set(range(c.get_Qbit_Num()))
@@ -99,3 +86,167 @@ def find_next_biggest_partition(c, max_qubits_per_partition, prevparts=None):
     qubits = set.union(*(get_qubits(gatedict[i]) for i in gates))
     #print(f"Status: {pulp.LpStatus[prob.status]}  Found partition with {len(gates)} gates: {gates} and {len(qubits)} qubits: {qubits}")
     return gates
+
+def nuutila_reach_scc(succ, subg=None):
+  index, s, sc, sccs, reach = 0, [], [], {}, {}
+  indexes, lowlink, croot, stackheight = {}, {}, {}, {}
+  def nuutila(v, index):
+    #index/D, lowlink/CCR/component candidate root, final component/C, component stack height/H
+    stack = [(v, None, iter(succ[v]))]
+    while len(stack) != 0:
+      v, w, succv = stack.pop()
+      if w is None:
+        indexes[v], lowlink[v] = index, v
+        stackheight[v] = len(sc)
+        index += 1
+        s.append(v)
+      elif not w in croot:
+        if indexes[lowlink[w]] < indexes[lowlink[v]]: lowlink[v] = lowlink[w]
+      else: sc.append(croot[w])
+      for w in succv:
+        if not subg is None and not w in subg: continue
+        forward_edge = False
+        if not w in indexes: stack.append((v, w, succv)); stack.append((w, None, iter(succ[w]))); break #index = nuutila(w, index)
+        else: forward_edge = indexes[v] < indexes[w]
+        if not w in croot:
+          if indexes[lowlink[w]] < indexes[lowlink[v]]: lowlink[v] = lowlink[w]
+        elif not forward_edge: #(v, w) is not a forward edge - whether w on stack or not...
+          sc.append(croot[w])
+      else:
+        if lowlink[v] == v:
+          sccs[v] = set()
+          is_self_loop = s[-1] != v or v in succ[v]
+          while True:
+            w = s.pop()
+            sccs[v].add(w)
+            if w == v: break
+          reach[v] = set(sccs[v]) if is_self_loop else set()
+          if not subg is None and len(subg) == len(sccs[v]): return index
+          for x in sccs[v]: croot[x] = v
+          l = set()
+          while len(sc) != stackheight[v]:
+            x = sc.pop()
+            l.add(x)
+            for x in sorted(l, reverse=True, key=lambda y: indexes[y]):
+              if not x in reach[v]:
+                reach[v] |= reach[x]; reach[v] |= sccs[x]
+    return index
+  for v in succ:
+    if (subg is None or v in subg) and not v in indexes:
+      index = nuutila(v, index)
+  return sccs, reach #keys are SCCs, values are reachable vertices
+
+def _get_topo_order(g, rg):
+    g = { x: set(y) for x, y in g.items() }
+    rg = { x: set(y) for x, y in rg.items() }
+    S = {m for m in rg if len(rg[m]) == 0}
+    L = []
+    while S:
+        n = S.pop()
+        L.append(n)
+        assert not rg[n]
+        for m in set(g[n]):
+            g[n].remove(m)
+            rg[m].remove(n)
+            if not rg[m]:
+                S.add(m)
+    return L
+
+def get_float_ops(num_qubit, gate_qubits, control_qubits):
+    g_size = 2**(gate_qubits-control_qubits)
+    # (a + bi) * (c + di) = (ac - bd) + (ad + bc)i => 6 ops for 4m2a
+    return 2**(num_qubit-control_qubits) * (g_size * (4 + 2) + 2 * (g_size - 1))
+
+def contract_single_qubit_chains(g, rg, gate_to_qubit, topo_order):
+    # Identify and contract single-qubit chains in the circuit
+    g = { x: set(y) for x, y in g.items() }
+    rg = { x: set(y) for x, y in rg.items() }
+    single_qubit_gates = {x for x, y in gate_to_qubit.items() if len(y) == 1}
+    single_qubit_chains = {}
+    for gate in topo_order:
+        if gate in single_qubit_gates:
+            # Contract the single-qubit chain
+            if rg[gate]:
+                v = next(iter(rg[gate]))
+                single_qubit_chains[gate] = single_qubit_chains[v] if v in single_qubit_chains else []
+                if not v in single_qubit_gates: g[v].remove(gate)
+            else: single_qubit_chains[gate] = []
+            single_qubit_chains[gate].append(gate)
+            if g[gate]:
+                v = next(iter(g[gate]))
+                if not v in single_qubit_gates: rg[v].remove(gate)
+            del g[gate]; del rg[gate]
+    topo_order = [x for x in topo_order if not x in single_qubit_gates]
+    return g, rg, topo_order, single_qubit_chains
+def recombine_single_qubit_chains(g, rg, single_qubit_chains, L):
+    L = [set(x) for x in L]
+    gate_to_part = {x: part for part in L for x in part}
+    for chain in set(tuple(x) for x in single_qubit_chains.values()):
+        if rg[chain[0]]:
+            v = next(iter(rg[chain[0]]))
+            gate_to_part[v] |= frozenset(chain)
+        elif g[chain[-1]]:
+            v = next(iter(g[chain[-1]]))
+            gate_to_part[v] |= frozenset(chain)
+        else:
+            L.append(frozenset(chain))
+    return L
+
+def max_partitions(c, max_qubits_per_partition):
+    gate_dict, go, rgo, gate_to_qubit, S = build_dependency(c)
+    topo_order = _get_topo_order(go, rgo)
+    g, rg, topo_order, single_qubit_chains = contract_single_qubit_chains(go, rgo, gate_to_qubit, topo_order)
+    topo_index = {x: i for i, x in enumerate(topo_order)} #all topological sorts of the DAG are the same as acyclic orderings of the transitive closure
+    _, reach = nuutila_reach_scc(g)
+    _, revreach = nuutila_reach_scc(rg)
+    #https://www.sciencedirect.com/science/article/pii/S1570866708000622
+    allparts = set()
+    for t in topo_index:
+        Y = set(topo_order[topo_index[t]+1:])
+        Anew, Bnew = reach[t] & Y, revreach[t] & Y
+        stack = [({t}, Y, Anew, Bnew, list(sorted(Anew, key=topo_index.__getitem__)), list(sorted(Bnew, key=topo_index.__getitem__, reverse=True)), gate_to_qubit[t])]
+        while stack:
+            X, Y, A, B, As, Bs, Q = stack.pop()
+            if A:
+                v = As.pop()
+                A.remove(v)
+                R = revreach[v] & A; R.add(v)
+            elif B:
+                v = Bs.pop()
+                B.remove(v)
+                R = reach[v] & B; R.add(v)
+            else: allparts.add(frozenset(X)); continue
+            Y.remove(v)
+            stack.append((X, Y, A, B, As, Bs, Q))
+            newQ = set(Q)
+            for x in R:
+                newQ |= gate_to_qubit[x]
+                if len(newQ) > max_qubits_per_partition: break
+            else:
+                Ynew, Anew, Bnew = Y - R, A - R, B - R
+                for x in R: Anew |= reach[x] & Ynew; Bnew |= revreach[x] & Ynew
+                stack.append((X | R, Ynew, Anew, Bnew, list(sorted(Anew, key=topo_index.__getitem__)), list(sorted(Bnew, key=topo_index.__getitem__, reverse=True)), newQ))
+    L, excluded = [], set()
+    for part in sorted(allparts, key=len, reverse=True):
+        if len(part & excluded) != 0: continue
+        excluded |= part
+        L.append(part)
+    L = recombine_single_qubit_chains(go, rgo, single_qubit_chains, L)
+    return topo_sort_partitions(c, max_qubits_per_partition, L)
+
+def _test_max_qasm():
+    K = 4
+    filename = "examples/partitioning/qasm_samples/heisenberg-16-20.qasm" 
+    #filename = "benchmarks/partitioning/test_circuit/0410184_169.qasm"
+    #filename = "benchmarks/partitioning/test_circuit/9symml_195.qasm"
+    #filename = "benchmarks/partitioning/test_circuit/adr4_197_qsearch.qasm"
+    
+    from squander import utils
+    
+    circ, parameters = utils.qasm_to_squander_circuit(filename)
+    partition = max_partitions(circ, K)
+
+    print(partition[2], len(partition[2]))
+
+if __name__ == "__main__":
+    _test_max_qasm()
