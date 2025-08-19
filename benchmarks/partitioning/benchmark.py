c84dc9bda93593ea00dba0f00211d12aa2258203
Gregory Morse <gregory.morse@live.com>
2025-08-19 19:20:41 +0200

    Update benchmark.py

--- benchmarks/partitioning/benchmark.py
+++ benchmarks/partitioning/benchmark.py
@@ -6,9 +6,9 @@ import timeit
 import glob
 import os
 
-USE_ILP = False
+USE_ILP = True
 
-MAX_GATES_ALLOWED = 4096 #1024**2 
+MAX_GATES_ALLOWED = 1024**2 
 
 METHOD_NAMES = [
     "kahn", 
@@ -24,15 +24,9 @@ METHOD_NAMES = [
 from squander.gates import gates_Wrapper as gate
 SUPPORTED_GATES = {x for n in dir(gate) for x in (getattr(gate, n),) if not n.startswith("_") and issubclass(x, gate.Gate) and n != "Gate"}
 SUPPORTED_GATES_NAMES = {n for n in dir(gate) if not n.startswith("_") and issubclass(getattr(gate, n), gate.Gate) and n != "Gate"}
+SUPPORTED_GATES_NAMES.remove("S")
 
 def test_partitions(max_qubits = 4):
-    """
-    Benchmark partitioning strategies on QASM circuits
-
-    Args:
-        
-        max_qubits: Max qubits per partition
-    """
     files = glob.glob("benchmarks/partitioning/test_circuit/*.qasm")
     print(f"Total QASM: {len(files)}, max qubits: {max_qubits}")
     allfiles = {}
@@ -44,10 +38,10 @@ def test_partitions(max_qubits = 4):
         if num_gates > MAX_GATES_ALLOWED or not qc_gates_names.issubset(SUPPORTED_GATES_NAMES):
             print(f"Skipping {fname}; qubits {qc.num_qubits} gates {qc_gates_names} num_gates {num_gates}")
             continue
-        print(fname)
+        print(fname, num_gates, max_qubits)
         res = {}
         for method in METHOD_NAMES:
-            print(method, fname, num_gates)
+            print(method)
             ls = []
             def f():
                 ls.extend(PartitionCircuitQasm( filename, max_qubits, method ))
@@ -61,13 +55,14 @@ def test_partitions(max_qubits = 4):
     import matplotlib.pyplot as plt
     sorted_items = sorted(allfiles.items(), key=lambda item: (item[1][0], item[1][1]))
     circuits_sorted = [name for name, _ in sorted_items]
+    circuits_labels = [name.replace(".qasm", f" ({x[0]}, {x[1]})") for name, x in sorted_items]
     markers = ["o", "*", "D", "s", "+", "<", ">", "v", "^"]
     for perf in (True, False):
-        title = "Partition_count" if not perf else "Performance"
+        title = "Partition Count" if not perf else "Runtime Performance"
         y_label = "Partition Count" if not perf else "Time in seconds"
         for i, strat in enumerate(METHOD_NAMES):
             y = [allfiles[name][2][strat][1 if perf else 0] for name in circuits_sorted]
-            plt.plot(circuits_sorted, y, marker=markers[i], label=strat)
+            plt.plot(circuits_labels, y, marker=markers[i], label=strat)
         plt.xlabel("Circuit (sorted by qubits, gates)")
         plt.ylabel(y_label)
         plt.title(f"{y_label} - {max_qubits} per Circuit by Strategy")
@@ -75,7 +70,7 @@ def test_partitions(max_qubits = 4):
         plt.legend()
         plt.tight_layout()
         plt.grid(True)
-        plt.savefig(f"{title}-{max_qubits}-max_qubit.svg", format="svg", transparent=True)
+        plt.savefig(f"{title.replace(' ', '_')}-{max_qubits}-max_qubit.svg", format="svg", transparent=True)
         plt.clf()
 
 if __name__ == "__main__":
