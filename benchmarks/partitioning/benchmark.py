402b0079793742e76e166aea5ed6d831289fa063
Gregory Morse <gregory.morse@live.com>
2025-08-20 03:20:26 +0200

    rewrote benchmark for better labels, latex table generation, filter custom files

--- benchmarks/partitioning/benchmark.py
+++ benchmarks/partitioning/benchmark.py
@@ -41,10 +41,10 @@ def test_partitions(max_qubits = 4):
         qc_gates_names = {qiskit_to_squander_name(inst.operation.name) for inst in qc.data}
         num_gates = len(qc.data)
         fname = os.path.basename(filename)      
-        if num_gates > MAX_GATES_ALLOWED or not qc_gates_names.issubset(SUPPORTED_GATES_NAMES):
+        if num_gates > MAX_GATES_ALLOWED or not qc_gates_names.issubset(SUPPORTED_GATES_NAMES) or filename.endswith("_qsearch.qasm") or filename.endswith("_squander.qasm"):
             print(f"Skipping {fname}; qubits {qc.num_qubits} gates {qc_gates_names} num_gates {num_gates}")
             continue
-        print(fname, num_gates, max_qubits)
+        print(fname, qc.num_qubits, num_gates, f"k={max_qubits}")
         res = {}
         for method in METHOD_NAMES:
             print(method)
@@ -57,11 +57,22 @@ def test_partitions(max_qubits = 4):
         allfiles[fname] = (qc.num_qubits, num_gates, res)
         print(fname, allfiles[fname])
     import json
-    print(json.dumps(allfiles, indent=2))
+    print(json.dumps(allfiles, indent=2))    
     import matplotlib.pyplot as plt
     sorted_items = sorted(allfiles.items(), key=lambda item: (item[1][0], item[1][1]))
     circuits_sorted = [name for name, _ in sorted_items]
     circuits_labels = [name.replace(".qasm", f" ({x[0]}, {x[1]})") for name, x in sorted_items]
+    print("Circuit Name & Qubit Count & Gate Count & " + " & ".join(METHOD_NAMES) + r"\\")
+    if USE_ILP:
+        ilpbest = [name for name in circuits_sorted if allfiles[name][2]["ilp"][0] != min(allfiles[name][2][method][0] for method in METHOD_NAMES if method != "ilp")]
+        ilptie = [name for name in circuits_sorted if allfiles[name][2]["ilp"][0] == min(allfiles[name][2][method][0] for method in METHOD_NAMES if method != "ilp")]
+        print("\n".join(" & ".join((name.replace(".qasm", "").replace("_", r"\_"), str(allfiles[name][0]), str(allfiles[name][1]),
+                                    *((r"\textbf{" if allfiles[name][2][method][0] == m else "") + str(allfiles[name][2][method][0]) + (r"}" if allfiles[name][2][method][0] == m else "") for method in METHOD_NAMES))) + r"\\" for name in ilpbest for m in (min(allfiles[name][2][method][0] for method in METHOD_NAMES),)))
+        print("\n".join(" & ".join((name.replace(".qasm", "").replace("_", r"\_"), str(allfiles[name][0]), str(allfiles[name][1]),
+                                    *((r"\textbf{" if allfiles[name][2][method][0] == m else "") + str(allfiles[name][2][method][0]) + (r"}" if allfiles[name][2][method][0] == m else "") for method in METHOD_NAMES))) + r"\\" for name in ilptie for m in (min(allfiles[name][2][method][0] for method in METHOD_NAMES),)))
+    else:
+        print("\n".join(" & ".join((name.replace(".qasm", "").replace("_", r"\_"), str(allfiles[name][0]), str(allfiles[name][1]),
+                                    *((r"\textbf{" if allfiles[name][2][method][0] == m else "") + str(allfiles[name][2][method][0]) + (r"}" if allfiles[name][2][method][0] == m else "") for method in METHOD_NAMES))) + r"\\" for name in circuits_sorted for m in (min(allfiles[name][2][method][0] for method in METHOD_NAMES),)))
     markers = ["o", "*", "D", "s", "+", "<", ">", "v", "^"]
     for perf in (True, False):
         title = "Partition Count" if not perf else "Runtime Performance"
@@ -70,8 +81,8 @@ def test_partitions(max_qubits = 4):
             y = [allfiles[name][2][strat][1 if perf else 0] for name in circuits_sorted]
             plt.plot(circuits_labels, y, marker=markers[i], label=strat)
         plt.xlabel("Circuit (sorted by qubits, gates)")
-        plt.ylabel(y_label)
-        plt.title(f"{y_label} - {max_qubits} per Circuit by Strategy")
+        plt.ylabel(y_label, fontsize=6)
+        plt.title(f"{y_label} - Maximum k={max_qubits} Qubits per Circuit")
         plt.xticks(rotation=45, ha="right")
         plt.legend()
         plt.tight_layout()
