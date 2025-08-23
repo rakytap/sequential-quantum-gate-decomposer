8f1d486d00774e2428584ec5c2efa82b63fe0afa
Gregory Morse <gregory.morse@live.com>
2025-08-24 01:32:08 +0200

    Update benchmark.py

--- benchmarks/partitioning/benchmark.py
+++ benchmarks/partitioning/benchmark.py
@@ -1,3 +1,4 @@
+import itertools
 from squander.partitioning.partition import PartitionCircuitQasm
 from qiskit import QuantumCircuit
 from squander.partitioning.tools import qiskit_to_squander_name, get_qubits, total_float_ops
@@ -28,6 +29,99 @@ SUPPORTED_GATES = {x for n in dir(gate) for x in (getattr(gate, n),) if not n.st
 SUPPORTED_GATES_NAMES = {n for n in dir(gate) if not n.startswith("_") and issubclass(getattr(gate, n), gate.Gate) and n != "Gate"}
 SUPPORTED_GATES_NAMES.remove("S")
 
+def purity_analysis():
+    import sympy
+    from sympy.combinatorics import Permutation
+    theta, phi, lbda = sympy.Symbol("θ"), sympy.Symbol("ϕ"), sympy.Symbol("λ")
+    theta2 = sympy.Symbol("θ2")
+    little_endian = True
+    def find_control_qubits(psi, num_qubits):
+        pure_controls = []
+        sparsity_controls = []
+        for i in range(1, 1<<num_qubits):
+            is_pure, is_sparse = True, True
+            for j in range(1<<num_qubits):
+                if i & j != i:
+                    if not all(psi[j, k] == (1 if j == k else 0) for k in range(1<<num_qubits)):
+                        is_pure = False
+                    if not all(psi[j, k] == 0 for k in range(1<<num_qubits) if i & k == i):
+                        is_sparse = False
+            if is_pure: pure_controls.append([j if little_endian else num_qubits-1-j for j in range(num_qubits) if i & (1<<j)])
+            if is_sparse: sparsity_controls.append([j if little_endian else num_qubits-1-j for j in range(num_qubits) if i & (1<<j)])
+        return pure_controls, sparsity_controls
+    def apply_to(psi, num_qubits, gate, gate_qubits):
+        pos = [(q if little_endian else num_qubits-1-q) for q in gate_qubits]
+        k = len(gate_qubits)
+        deltas, combo = [1<<p for p in pos], []
+        for u in range (1 << k):
+            m = 0; x = u; b = 0
+            while x:
+                if x & 1: m ^= deltas[b]
+                x >>= 1; b += 1
+            combo.append(m)
+        pos_set = set(pos)
+        rest = [p for p in range(num_qubits) if not p in pos_set]
+        output = sympy.zeros(1<<num_qubits, 1<<num_qubits)
+        for rest_bits in itertools.product((0, 1), repeat=len(rest)):
+            base = 0
+            for bit, p in zip(rest_bits, rest):
+                if bit: base |= 1<<p
+            mat, idx = sympy.zeros(0, 1<<num_qubits), []
+            for u in range(1 << k):
+                idx.append(base ^ combo[u])
+                mat = mat.col_join(psi[idx[u],:])
+            prod = gate @ mat
+            for u in range(1 << k):
+                output[idx[u],:] = prod[u,:]
+        return output
+    def compile_gates(num_qubits, gates):
+        Umtx = sympy.eye(2**num_qubits)
+        for gate in gates: Umtx = apply_to(Umtx, num_qubits, *gate)
+        Umtx.simplify()
+        return Umtx
+    def make_controlled(gate, gate_qubits, gateother=None): #control is first qubit, gate qubits come after
+        res = sympy.diag(gateother if not gateother is None else sympy.eye(1<<gate_qubits), gate)
+        P = sympy.eye(1<<(gate_qubits+1))[:, [2*x+y for y in (0, 1) for x in range(1<<gate_qubits)]]
+        return P * res * P.T if little_endian else res
+    def gen_Ry(theta): return sympy.Matrix([[sympy.cos(theta/2.0), -sympy.sin(theta/2.0)], [sympy.sin(theta/2.0), sympy.cos(theta/2.0)]])
+    def gen_Rz(phi): return sympy.Matrix([[sympy.exp(-1j*phi/2.0), 0], [0, sympy.exp(1j*phi/2.0)]])
+    def gen_Rx(lbda): return sympy.Matrix([[sympy.cos(lbda/2.0), -1j*sympy.sin(lbda/2.0)], [-1j*sympy.sin(lbda/2.0), sympy.cos(lbda/2.0)]])
+    def gen_GP(theta): return sympy.exp(theta*1j)
+    def gen_H(): return sympy.Matrix([[1, 1], [1, -1]])/sympy.sqrt(2)
+    def gen_X(): return sympy.Matrix([[0, 1], [1, 0]])
+    def gen_Y(): return sympy.Matrix([[0, -1j], [1j, 0]])
+    def gen_Z(): return sympy.Matrix([[1, 0], [0, -1]])
+    def gen_S(): return sympy.Matrix([[1, 0], [0, 1j]])
+    def gen_Sdg(): return sympy.Matrix([[1, 0], [0, -1j]])
+    def gen_SX(): return sympy.Matrix([[1+1j, 1-1j], [1-1j, 1+1j]])/2
+    def gen_SYC(): return sympy.Matrix([[1, 0, 0, 0], [0, 0, -1j, 0], [0, -1j, 0, 0], [0, 0, 0, sympy.exp(-1j*sympy.pi/6)]])
+    def gen_T(): return sympy.Matrix([[1, 0], [0, sympy.exp(1j*sympy.pi/4)]])
+    def gen_Tdg(): return sympy.Matrix([[1, 0], [0, sympy.exp(-1j*sympy.pi/4)]])
+    def gen_P(theta): return sympy.Matrix([[1, 0], [0, sympy.exp(1j*theta)]])
+    def gen_U1(theta): return gen_P(theta)
+    def gen_CH(): return sympy.Matrix(make_controlled(gen_H(), 1))
+    def gen_CX(): return sympy.Matrix(make_controlled(gen_X(), 1))
+    def gen_CNOT(): return gen_CX()
+    def gen_CY(): return sympy.Matrix(make_controlled(gen_Y(), 1))
+    def gen_CZ(): return sympy.Matrix(make_controlled(gen_Z(), 1))
+    def gen_U(theta, phi, lbda): u = (gen_Rz(phi) @ gen_Ry(theta) @ gen_Rz(lbda) * gen_GP((phi+lbda)/2)); u.simplify(); return u
+    def gen_U2(phi, lbda): return gen_U(sympy.pi/2, phi, lbda)
+    def gen_U3(theta, phi, lbda): return gen_U(theta, phi, lbda)
+    def gen_R(theta, phi): return gen_U(theta, phi-sympy.pi/2, -phi+sympy.pi/2)
+    def gen_CR(theta, phi): return make_controlled(gen_R(theta, phi), 1)
+    def gen_CROT(theta, phi): return make_controlled(gen_R(theta, phi), 1, gen_R(-theta, phi))
+    def gen_CRY(theta): return make_controlled(gen_Ry(theta), 1)
+    def gen_CCX(): return make_controlled(gen_CNOT(), 2)
+    print(find_control_qubits(gen_U3(theta, phi, lbda), 1), find_control_qubits(gen_CRY(theta), 2), find_control_qubits(gen_CCX(), 3))
+    for i in range(3): #this proves any single qubit chain removes all purity, and converts aligning control to target
+        print(f"U3({i})@CRY(0, 1) pure, sparse control:", find_control_qubits(compile_gates(3, [(gen_U3(theta, phi, lbda), [i]), (gen_CRY(theta2), (0, 1))]), 3))
+    for i in range(3):
+        for j in range(3):
+            if i == j : continue
+            print(f"CRY({i}, {j})@CRY(0, 1) pure, sparse control:", find_control_qubits(compile_gates(3, [(gen_CRY(theta), [i, j]), (gen_CRY(theta2), (0, 1))]), 3))
+
+#purity_analysis(); assert False
+
 def test_partitions(max_qubits = 4):
     """
     Benchmark partitioning strategies on QASM circuits
@@ -42,7 +136,7 @@ def test_partitions(max_qubits = 4):
         qc = QuantumCircuit.from_qasm_file(filename)
         qc_gates_names = {qiskit_to_squander_name(inst.operation.name) for inst in qc.data}
         num_gates = len(qc.data)
-        fname = os.path.basename(filename)      
+        fname = os.path.basename(filename)
         if num_gates > MAX_GATES_ALLOWED or not qc_gates_names.issubset(SUPPORTED_GATES_NAMES) or filename.endswith("_qsearch.qasm") or filename.endswith("_squander.qasm"):
             print(f"Skipping {fname}; qubits {qc.num_qubits} gates {qc_gates_names} num_gates {num_gates}")
             continue
