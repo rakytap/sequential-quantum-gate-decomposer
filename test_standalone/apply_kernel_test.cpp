0c0418c7086db4ffbcb6ce8cab1f0be350003a30
alfaarizi <ziziyusrizal@gmail.com>
2025-09-04 18:31:44 -0400

    test: add gate fusion fidelity test for small circuits

--- test_standalone/apply_kernel_test.cpp
+++ test_standalone/apply_kernel_test.cpp
@@ -2,6 +2,7 @@
 #include <random>
 #include <cassert>
 #include "matrix.h"
+#include "Gates_block.h"
 #include "common.h"
 #include "apply_large_kernel_to_input.h"
 
@@ -94,14 +95,59 @@ void test5QubitGate() {
     assert(std::abs(fid - 1.0) < 1e-10);
 }
 
+void testSmallCircuit() {
+    int num_qubits = 4;
+
+    Matrix state = generateRandomState(num_qubits);
+    Matrix test_state = state.copy();
+
+    Gates_block circuit(num_qubits);
+    Gates_block* circuit_inner = new Gates_block(num_qubits);
+    Gates_block* circuit_inner_2 = new Gates_block(num_qubits);
+    
+    circuit_inner->add_u3(0);
+    circuit_inner->add_u3(1);
+    circuit_inner->add_u3(2);
+
+    circuit_inner->add_cry(0, 1);
+    circuit_inner->add_cry(1, 2);
+    circuit_inner_2->add_cry(2, 3);
+
+    circuit_inner_2->add_u3(3);
+    circuit_inner_2->add_u3(0);
+
+    // circuit.fragment_circuit();
+    circuit.add_gate(circuit_inner);
+    circuit.add_gate(circuit_inner_2);
+
+    int num_params = circuit.get_parameter_num();
+    Matrix_real parameters(num_params, 1);
+    for (int i = 0; i < num_params; i++) {
+        parameters[i] = (i+1) / (M_PI*2);
+    }
+
+    circuit.apply_to(parameters, state);
+
+    // circuit.set_max_fusion(0);
+    circuit_inner->set_max_fusion(0);
+    circuit_inner_2->set_max_fusion(0);
+
+    circuit.apply_to(parameters, test_state);
+
+    double fid = fidelity(state, test_state);
+    std::cout << "Identity circuit fidelity (should be 1.0): " << fid << std::endl;
+    assert(std::abs(fid - 1.0) < 1e-10);
+}
+
 
 };
 
 int main() {
     ApplyKernelTestSuite suite;
-    suite.test2QubitGate();
-    suite.test3QubitGate();
-    suite.test4QubitGate();
-    suite.test5QubitGate();
+    suite.testSmallCircuit();
+    // suite.test2QubitGate();
+    // suite.test3QubitGate();
+    // suite.test4QubitGate();
+    // suite.test5QubitGate();
     return 0;
 }
