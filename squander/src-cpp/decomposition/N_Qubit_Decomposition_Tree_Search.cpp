7257e5fd59c0410b291df3989c6f33d2cf8a0419
JNadori <degututaj@gmail.com>
2025-10-22 10:33:07 +0200

    Set construct circuit from gray code to be more consistent only using U3-s

--- squander/src-cpp/decomposition/N_Qubit_Decomposition_Tree_Search.cpp
+++ squander/src-cpp/decomposition/N_Qubit_Decomposition_Tree_Search.cpp
@@ -728,11 +728,11 @@ N_Qubit_Decomposition_Tree_Search::add_finalyzing_layer( Gates_block* gate_struc
     block->add_ry(qbit_num-1);
 */
     for (int idx=0; idx<qbit_num; idx++) {
-        block->add_rz(idx);
-        block->add_ry(idx);
-        block->add_rz(idx); 
+        //block->add_rz(idx);
+        //block->add_ry(idx);
+        //block->add_rz(idx); 
 
-             //block->add_u3(idx, Theta, Phi, Lambda);
+        block->add_u3(idx);
 //        block->add_ry(idx);
     }
 
