887d384e51d967d72bdfd462278c4bd2c6466ad3
Gregory Morse <gregory.morse@live.com>
2025-10-26 08:55:35 +0100

    Fix typo in cost function type definition

--- squander/src-cpp/decomposition/include/Optimization_Interface.h
+++ squander/src-cpp/decomposition/include/Optimization_Interface.h
@@ -37,10 +37,10 @@ limitations under the License.
 
 
 
-/// @brief Type definition of the fifferent types of the cost function
+/// @brief Type definition of the different types of the cost function
 typedef enum cost_function_type {FROBENIUS_NORM, FROBENIUS_NORM_CORRECTION1, FROBENIUS_NORM_CORRECTION2,
     HILBERT_SCHMIDT_TEST, HILBERT_SCHMIDT_TEST_CORRECTION1, HILBERT_SCHMIDT_TEST_CORRECTION2,
-    SUM_OF_SQUARES, VQE, GQML, INFIDELITY} cost_function_type;
+    SUM_OF_SQUARES, VQE, GQML, INFIDELITY, OSR_ENTANGLEMENT} cost_function_type;
 
 
 
