bd16cb40740903c697116da6d47785a0964ca3d6
JNadori <degututaj@gmail.com>
2025-10-29 12:55:42 +0100

    Add partition aware mapping synthesis part

---
+++ squander/synthesis/PartAM.py
@@ -0,0 +1,177 @@
+"""
+This is an implementation of Partition Aware Mapping.
+"""
+from squander.decomposition.qgd_N_Qubit_Decompositions_Wrapper import (
+    qgd_N_Qubit_Decomposition_adaptive as N_Qubit_Decomposition_adaptive,
+    qgd_N_Qubit_Decomposition_Tree_Search as N_Qubit_Decomposition_Tree_Search,
+    qgd_N_Qubit_Decomposition_Tabu_Search as N_Qubit_Decomposition_Tabu_Search,
+)
+from squander.gates.qgd_Circuit import qgd_Circuit as Circuit
+from itertools import permutations
+from squander.partitioning.ilp import get_all_partitions, _get_topo_order, topo_sort_partitions, ilp_global_optimal, recombine_single_qubit_chains
+
+import numpy as np
+from qiskit import QuantumCircuit
+
+from typing import List, Callable
+
+import multiprocessing as mp
+from multiprocessing import Process, Pool
+import os
+
+
+from squander.partitioning.partition import PartitionCircuit
+from squander.partitioning.tools import get_qubits
+from squander.synthesis.qgd_SABRE import qgd_SABRE as SABRE
+from itertools import product
+
+def extract_subtopology(involved_qbits, qbit_map, config ):
+    mini_topology = []
+    for edge in config["topology"]:
+        if edge[0] in involved_qbits and edge[1] in involved_qbits:
+            mini_topology.append((qbit_map[edge[0]],qbit_map[edge[1]]))
+    return mini_topology
+
+class PartitionSynthesisResult:
+    def __init__(self):
+        self.permutations_pairs = []
+        self.synthesised_circuits = []
+        self.synthesised_parameters = []
+        self.cnot_counts = []
+    
+    def add_result(self, permutations_pair, synthesised_circuit, synthesised_parameters):
+        self.permutations_pairs.append(permutations_pair)
+        self.synthesised_circuits.append(synthesised_circuit)
+        self.synthesised_parameters.append(synthesised_parameters)
+        self.cnot_counts.append(synthesised_circuit.get_Gate_Nums().get('CNOT', 0))
+    
+    def get_best_result(self):
+        best_index = np.argmin(self.cnot_counts)
+        return self.permutations_pairs[best_index], self.synthesised_circuits[best_index], self.synthesised_parameters[best_index]
+    
+    def get_partition_synthesis_score(self):
+        return np.mean(self.cnot_counts)*0.1 + np.min(self.cnot_counts)*0.9
+
+class qgd_Partition_Aware_Mapping:
+
+    def __init__(self, config):
+        self.config = config
+        self.config.setdefault('strategy', 'TreeSearch')
+        self.config.setdefault('parallel', 0 )
+        self.config.setdefault('verbosity', 0 )
+        self.config.setdefault('tolerance', 1e-8 )
+        self.config.setdefault('test_subcircuits', False )
+        self.config.setdefault('test_final_circuit', True )
+        self.config.setdefault('max_partition_size', 3 )
+        self.config.setdefault('topology', None)
+        self.config.setdefault('routed', False)
+        self.config.setdefault('partition_strategy','ilp')
+        self.config.setdefault('optimizer', 'BFGS')
+        strategy = self.config['strategy']
+        allowed_strategies = ['TreeSearch', 'TabuSearch', 'Adaptive']
+        if not strategy in allowed_strategies:
+            raise Exception(f"The strategy should be either of {allowed_strategies}, got {strategy}.")
+        parallel = self.config['parallel']
+        allowed_parallel = [0, 1, 2]
+        if not parallel in allowed_parallel:
+            raise Exception(f"The parallel configuration should be either of {allowed_parallel}, got {parallel}.")
+        verbosity = self.config['verbosity']
+        if not isinstance(verbosity, int):
+            raise Exception(f"The verbosity parameter should be an integer.")
+
+        self.max_partition_size = self.config['max_partition_size']
+        if not isinstance(self.max_partition_size, int):
+            raise Exception(f"The max_partition_size parameter should be an integer.")
+        self.topology = self.config['topology']
+        if not isinstance(self.topology, list):
+            raise Exception(f"The topology parameter should be a list.")
+        self.routed = self.config['routed']
+        if not isinstance(self.routed, bool):
+            raise Exception(f"The routed parameter should be a bool.")
+        self.partition_strategy = self.config['partition_strategy']
+        allowed_partition_strategies = ['ilp', 'tdag', 'kahn', 'qiskit', 'qiskit-fusion', 'bqskit-Quick', 'bqskit-Scan', 'bqskit-Greedy', 'bqskit-Cluster']
+    
+    @staticmethod
+    def DecomposePartition_and_Perm(Umtx: np.ndarray, config: dict, mini_topology = None) -> Circuit:
+        """
+        Call to decompose a partition
+        """
+        strategy = config["strategy"]
+        if strategy == "TreeSearch":
+            cDecompose = N_Qubit_Decomposition_Tree_Search( Umtx.conj().T, config=config, accelerator_num=0, topology=mini_topology)
+        elif strategy == "TabuSearch":
+            cDecompose = N_Qubit_Decomposition_Tabu_Search( Umtx.conj().T, config=config, accelerator_num=0, topology=mini_topology )
+        elif strategy == "Adaptive":
+            cDecompose = N_Qubit_Decomposition_adaptive( Umtx.conj().T, level_limit_max=5, level_limit_min=1, topology=mini_topology )
+        else:
+            raise Exception(f"Unsupported decomposition type: {strategy}")
+        cDecompose.set_Verbose( config["verbosity"] )
+        cDecompose.set_Cost_Function_Variant( 3 )	
+        cDecompose.set_Optimization_Tolerance( config["tolerance"] )
+        cDecompose.set_Optimizer( config["optimizer"] )
+        cDecompose.Start_Decomposition()
+        squander_circuit = cDecompose.get_Circuit()
+        parameters       = cDecompose.get_Optimized_Parameters()
+        return squander_circuit, parameters
+
+    @staticmethod
+    def DecomposePartition_Sequential(Partition_circuit: Circuit, Partition_parameters: np.ndarray, config: dict, mini_topology = None) -> PartitionSynthesisResult:
+        """
+        Call to decompose a partition sequentially
+        """
+        N = Partition_circuit.get_Qbit_Num()
+        perumations_all = permutations(range(N))
+        N_permutations = len(perumations_all)
+        result = PartitionSynthesisResult()
+        # Sequential permutation search
+        P_o_initial = np.random.choice(perumations_all)
+        for P_i in perumations_all:
+            Partition_circuit_tmp = Circuit(N)
+            Partition_circuit_tmp.add_Permutation(P_i)
+            Partition_circuit_tmp.add_Circuit(Partition_circuit)
+            Partition_circuit_tmp.add_Permutation(P_o_initial)
+            synthesised_circuit, synthesised_parameters = DecomposePartition_and_Perm(Partition_circuit_tmp.get_Matrix(Partition_parameters), config, mini_topology)
+            result.add_result((P_i, P_o_initial), synthesised_circuit, synthesised_parameters)
+
+        P_i_best, _ = result.get_best_result()[0]
+        for P_o in perumations_all:
+            Partition_circuit_tmp = Circuit(N)
+            Partition_circuit_tmp.add_Permutation(P_i_best)
+            Partition_circuit_tmp.add_Circuit(Partition_circuit)
+            Partition_circuit_tmp.add_Permutation(P_o)
+            synthesised_circuit, synthesised_parameters = DecomposePartition_and_Perm(Partition_circuit_tmp.get_Matrix(Partition_parameters), config, mini_topology)
+            result.add_result((P_i_best, P_o), synthesised_circuit, synthesised_parameters)
+        return result
+
+    def SynthesizeWideCircuit(self, circ, orig_parameters):
+        allparts, g, go, rgo, single_qubit_chains, gate_to_qubit, gate_to_tqubit = get_all_partitions(circ, self.max_partition_size)
+        qbit_num_orig_circuit = circ.get_Qbit_Num()
+        gate_dict = {i: gate for i, gate in enumerate(circ.get_Gates())}
+        single_qubit_chains_pre = {x[0]: x for x in single_qubit_chains if rgo[x[0]]}
+        single_qubit_chains_post = {x[-1]: x for x in single_qubit_chains if go[x[-1]]}
+        single_qubit_chains_prepost = {x[0]: x for x in single_qubit_chains if x[0] in single_qubit_chains_pre and x[-1] in single_qubit_chains_post}
+        partitined_circuit = Circuit( qbit_num_orig_circuit )
+        params = []
+        for part in allparts:
+            surrounded_chains = {t for s in part for t in go[s] if t in single_qubit_chains_prepost and go[single_qubit_chains_prepost[t][-1]] and next(iter(go[single_qubit_chains_prepost[t][-1]])) in part}
+            gates = frozenset.union(part, *(single_qubit_chains_prepost[v] for v in surrounded_chains))
+            #topo sort part + surrounded chains
+            c = Circuit( qbit_num_orig_circuit )
+            for gate_idx in _get_topo_order({x: go[x] & gates for x in gates}, {x: rgo[x] & gates for x in gates}):
+                c.add_Gate( gate_dict[gate_idx] )
+                start = gate_dict[gate_idx].get_Parameter_Start_Index()
+                params.append(orig_parameters[start:start + gate_dict[gate_idx].get_Parameter_Num()])
+            partitioned_circuit.add_Circuit(c)
+        for chain in single_qubit_chains:
+            c = Circuit( qbit_num_orig_circuit )
+            for gate_idx in chain:
+                c.add_Gate( gate_dict[gate_idx] )
+                start = gate_dict[gate_idx].get_Parameter_Start_Index()
+                params.append(orig_parameters[start:start + gate_dict[gate_idx].get_Parameter_Num()])
+            partitioned_circuit.add_Circuit(c)
+        parameters = np.concatenate(params, axis=0)
+
+        qbit_num_orig_circuit = circ.get_Qbit_Num()
+
+
+        subcircuits = partitioned_circuit.get_Gates()
