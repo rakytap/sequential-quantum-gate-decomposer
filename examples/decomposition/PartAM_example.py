9001704fd055d5549c353d4e04df49da9899096f
JNadori <degututaj@gmail.com>
2025-10-30 18:56:16 +0100

    Reowrk PartAM

---
+++ examples/decomposition/PartAM_example.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 26 14:42:56 2020
+Copyright 2020 Peter Rakyta, Ph.D.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+@author: Peter Rakyta, Ph.D.
+"""
+## \file wide_circuit_optimization.py
+## \brief Simple example python code demonstrating a wide circuit optimization
+
+import squander.decomposition.qgd_Wide_Circuit_Optimization as Wide_Circuit_Optimization
+from squander import Partition_Aware_Mapping
+from squander import utils
+from squander import Qiskit_IO
+import time
+from squander import Circuit
+import numpy as np
+if __name__ == '__main__':
+
+
+    config = {
+            'strategy': "TreeSearch",
+            'test_subcircuits': True,
+            'test_final_circuit': True,
+            'max_partition_size': 3,
+            'minimum_partition_size': 2,  # Minimum qubits per partition (default: 2)
+    }
+
+    filename = "benchmarks/qfast/5q/vqe.qasm"
+    start_time = time.time()
+
+    # load the circuit from a file
+    circ, parameters = utils.qasm_to_squander_circuit(filename)
+    config['topology'] = [
+    (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7),
+    (8, 9), (8, 10), (8, 11), (8, 12), (8, 13), (8, 14), (8, 15),
+    (0, 8),
+    ]
+    wide_circuit_optimizer = Partition_Aware_Mapping( config )
+    wide_circuit_optimizer.SynthesizeWideCircuit( circ, parameters )
+
+    print("--- %s seconds elapsed during optimization ---" % (time.time() - start_time))
+
+
--- squander/synthesis/PartAM.py
+++ squander/synthesis/PartAM.py
@@ -241,6 +241,13 @@ def extract_subtopology(involved_qbits, qbit_map, config ):
             mini_topology.append((qbit_map[edge[0]],qbit_map[edge[1]]))
     return mini_topology
 
+class SingleQubitPartitionResult:
+    def __init__(self,circuit_in,parameters_in):
+        self.circuit = circuit_in
+        self.parameters = parameters_in
+    def get_partition_synthesis_score(self):
+        return 0
+
 class PartitionSynthesisResult:
     def __init__(self, N , mini_topologies):
         self.mini_topologies = mini_topologies
@@ -291,58 +298,62 @@ class qgd_Partition_Aware_Mapping:
         allowed_strategies = ['TreeSearch', 'TabuSearch', 'Adaptive']
         if not strategy in allowed_strategies:
             raise Exception(f"The strategy should be either of {allowed_strategies}, got {strategy}.")
-        parallel = self.config['parallel']
-        allowed_parallel = [0, 1, 2]
-        if not parallel in allowed_parallel:
-            raise Exception(f"The parallel configuration should be either of {allowed_parallel}, got {parallel}.")
-        verbosity = self.config['verbosity']
-        if not isinstance(verbosity, int):
-            raise Exception(f"The verbosity parameter should be an integer.")
-
-        self.max_partition_size = self.config['max_partition_size']
-        if not isinstance(self.max_partition_size, int):
-            raise Exception(f"The max_partition_size parameter should be an integer.")
-        self.topology = self.config['topology']
-        if not isinstance(self.topology, list):
-            raise Exception(f"The topology parameter should be a list.")
-        self.routed = self.config['routed']
-        if not isinstance(self.routed, bool):
-            raise Exception(f"The routed parameter should be a bool.")
-        self.partition_strategy = self.config['partition_strategy']
-        allowed_partition_strategies = ['ilp', 'tdag', 'kahn', 'qiskit', 'qiskit-fusion', 'bqskit-Quick', 'bqskit-Scan', 'bqskit-Greedy', 'bqskit-Cluster']
-
     @staticmethod
     def DecomposePartition_Sequential(Partition_circuit: Circuit, Partition_parameters: np.ndarray, config: dict, topologies) -> PartitionSynthesisResult:
         """
         Call to decompose a partition sequentially
         """
         N = Partition_circuit.get_Qbit_Num()
-        perumations_all = list(permutations(range(N)))
-        result = PartitionSynthesisResult(N, topologies)
-        # Sequential permutation search
-        for topology_idx in range(len(topologies)):
-            mini_topology = topologies[topology_idx]
-            P_o_initial = perumations_all[np.random.choice(range(len(perumations_all)))]
-            for P_i in perumations_all:
-                Partition_circuit_tmp = Circuit(N)
-                Partition_circuit_tmp.add_Permutation(P_i)
-                Partition_circuit_tmp.add_Circuit(Partition_circuit)
-                Partition_circuit_tmp.add_Permutation(P_o_initial)
-                synthesised_circuit, synthesised_parameters = DecomposePartition_and_Perm(Partition_circuit_tmp.get_Matrix(Partition_parameters), config, mini_topology)
-                result.add_result((P_i, P_o_initial), synthesised_circuit, synthesised_parameters, topology_idx)
+        if N !=1:
+            perumations_all = list(permutations(range(N)))
+            result = PartitionSynthesisResult(N, topologies)
+            # Sequential permutation search
+            for topology_idx in range(len(topologies)):
+                mini_topology = topologies[topology_idx]
+                P_o_initial = perumations_all[np.random.choice(range(len(perumations_all)))]
+                for P_i in perumations_all:
+                    Partition_circuit_tmp = Circuit(N)
+                    Partition_circuit_tmp.add_Permutation(P_i)
+                    Partition_circuit_tmp.add_Circuit(Partition_circuit)
+                    Partition_circuit_tmp.add_Permutation(P_o_initial)
+                    synthesised_circuit, synthesised_parameters = qgd_Partition_Aware_Mapping.DecomposePartition_and_Perm(Partition_circuit_tmp.get_Matrix(Partition_parameters), config, mini_topology)
+                    result.add_result((P_i, P_o_initial), synthesised_circuit, synthesised_parameters, topology_idx)
 
-            P_i_best, _ = result.get_best_result(topology_idx)[0]
-            for P_o in perumations_all:
-                Partition_circuit_tmp = Circuit(N)
-                Partition_circuit_tmp.add_Permutation(P_i_best)
-                Partition_circuit_tmp.add_Circuit(Partition_circuit)
-                Partition_circuit_tmp.add_Permutation(P_o)
-                synthesised_circuit, synthesised_parameters = DecomposePartition_and_Perm(Partition_circuit_tmp.get_Matrix(Partition_parameters), config, mini_topology)
-                result.add_result((P_i_best, P_o), synthesised_circuit, synthesised_parameters, topology_idx)
+                P_i_best, _ = result.get_best_result(topology_idx)[0]
+                for P_o in perumations_all:
+                    Partition_circuit_tmp = Circuit(N)
+                    Partition_circuit_tmp.add_Permutation(P_i_best)
+                    Partition_circuit_tmp.add_Circuit(Partition_circuit)
+                    Partition_circuit_tmp.add_Permutation(P_o)
+                    synthesised_circuit, synthesised_parameters = qgd_Partition_Aware_Mapping.DecomposePartition_and_Perm(Partition_circuit_tmp.get_Matrix(Partition_parameters), config, mini_topology)
+                    result.add_result((P_i_best, P_o), synthesised_circuit, synthesised_parameters, topology_idx)
+        else:
+            result = SingleQubitPartitionResult(Partition_circuit,Partition_parameters)
         return result
-
+    @staticmethod
+    def DecomposePartition_and_Perm(Umtx: np.ndarray, config: dict, mini_topology = None) -> Circuit:
+        """
+        Call to decompose a partition
+        """
+        strategy = config["strategy"]
+        if strategy == "TreeSearch":
+            cDecompose = N_Qubit_Decomposition_Tree_Search( Umtx.conj().T, config=config, accelerator_num=0, topology=mini_topology)
+        elif strategy == "TabuSearch":
+            cDecompose = N_Qubit_Decomposition_Tabu_Search( Umtx.conj().T, config=config, accelerator_num=0, topology=mini_topology )
+        elif strategy == "Adaptive":
+            cDecompose = N_Qubit_Decomposition_adaptive( Umtx.conj().T, level_limit_max=5, level_limit_min=1, topology=mini_topology )
+        else:
+            raise Exception(f"Unsupported decomposition type: {strategy}")
+        cDecompose.set_Verbose( config["verbosity"] )
+        cDecompose.set_Cost_Function_Variant( 3 )	
+        cDecompose.set_Optimization_Tolerance( config["tolerance"] )
+        cDecompose.set_Optimizer( config["optimizer"] )
+        cDecompose.Start_Decomposition()
+        squander_circuit = cDecompose.get_Circuit()
+        parameters       = cDecompose.get_Optimized_Parameters()
+        return squander_circuit, parameters
     def SynthesizeWideCircuit(self, circ, orig_parameters):
-        allparts, g, go, rgo, single_qubit_chains, gate_to_qubit, gate_to_tqubit = get_all_partitions(circ, self.max_partition_size)
+        allparts, g, go, rgo, single_qubit_chains, gate_to_qubit, gate_to_qubit = get_all_partitions(circ, self.config["max_partition_size"])
         qbit_num_orig_circuit = circ.get_Qbit_Num()
         gate_dict = {i: gate for i, gate in enumerate(circ.get_Gates())}
         single_qubit_chains_pre = {x[0]: x for x in single_qubit_chains if rgo[x[0]]}
@@ -360,6 +371,7 @@ class qgd_Partition_Aware_Mapping:
                 start = gate_dict[gate_idx].get_Parameter_Start_Index()
                 params.append(orig_parameters[start:start + gate_dict[gate_idx].get_Parameter_Num()])
             partitioned_circuit.add_Circuit(c)
+        # Only add single-qubit chains as separate partitions if minimum_partition_size allows it
         for chain in single_qubit_chains:
             c = Circuit( qbit_num_orig_circuit )
             for gate_idx in chain:
@@ -383,42 +395,18 @@ class qgd_Partition_Aware_Mapping:
                 end_idx   = subcircuit.get_Parameter_Start_Index() + subcircuit.get_Parameter_Num()
                 subcircuit_parameters = parameters[ start_idx:end_idx ]
                 k = subcircuit.get_Qbit_Num()
-                mini_topologies = get_unique_subtopologies(self.topology, k)
-                optimized_results[partition_idx] = pool.apply_async( self.DecomposePartition_Sequential, (subcircuit, subcircuit_parameters, self.config, mini_topologies) )
+                qbit_num_orig_circuit = subcircuit.get_Qbit_Num()
+                involved_qbits = subcircuit.get_Qbits()
+
+                qbit_num = len( involved_qbits )
+                mini_topologies = get_unique_subtopologies(self.topology, qbit_num)
+                qbit_map = {}
+                for idx in range( len(involved_qbits) ):
+                    qbit_map[ involved_qbits[idx] ] = idx
+                remapped_subcircuit = subcircuit.Remap_Qbits( qbit_map, qbit_num )
+                optimized_results[partition_idx] = pool.apply_async( self.DecomposePartition_Sequential, (remapped_subcircuit, subcircuit_parameters, self.config, mini_topologies) )
 
             for partition_idx, subcircuit in enumerate( tqdm(subcircuits, desc="Processing partitions") ):
                 optimized_results[partition_idx] = optimized_results[partition_idx].get()
 
         weights = [result.get_partition_synthesis_score() for result in optimized_results[:len(allparts)]]
-        L, fusion_info = ilp_global_optimal(allparts, g, weights=weights)
-        # Create a mapping from partition frozensets to their indices in allparts
-        partition_to_idx = {frozenset(part): i for i, part in enumerate(allparts)}
-
-        # Convert the returned partitions to indices
-        L_indices = [partition_to_idx[frozenset(part)] for part in L_parts]
-
-        # Now directly select the already-optimized subcircuits using the indices
-        selected_optimized_subcircuits = [optimized_subcircuits[i] for i in L_indices]
-
-  max_gates = max(len(c.get_Gates()) for c in optimized_subcircuits)
-  def to_cost(d): return d.get('CNOT', 0)*max_gates + sum(d[x] for x in d if x != 'CNOT')
-  weights = [to_cost(circ.get_Gate_Nums()) for circ in optimized_subcircuits[:len(allparts)]]
-
-  # ilp_global_optimal returns the selected partitions as frozensets
-  L_parts, fusion_info = ilp_global_optimal(allparts, g, weights=weights)
-
-  # Create a mapping from partition frozensets to their indices in allparts
-  partition_to_idx = {frozenset(part): i for i, part in enumerate(allparts)}
-
-  # Convert the returned partitions to indices
-  L_indices = [partition_to_idx[frozenset(part)] for part in L_parts]
-
-  # Now directly select the already-optimized subcircuits using the indices
-  selected_optimized_subcircuits = [optimized_subcircuits[i] for i in L_indices]
-  selected_parameters = [optimized_parameter_list[i] for i in L_indices]
-
-  # Construct the final circuit from the optimized subcircuits
-  wide_circuit, wide_parameters = self.ConstructCircuitFromPartitions(
-      selected_optimized_subcircuits,
-      selected_parameters
-  )
