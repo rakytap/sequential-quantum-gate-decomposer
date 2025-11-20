01737edfc51f5c725323d89d9595641bbd91d0f4
jnadori <degututaj@gmail.com>
2025-11-20 17:47:17 +0100

    Add state checking to example

--- examples/decomposition/PartAM_example.py
+++ examples/decomposition/PartAM_example.py
@@ -41,13 +41,31 @@ if __name__ == '__main__':
     start_time = time.time()
 
     # load the circuit from a file
-    circ, parameters = utils.qasm_to_squander_circuit(filename)
+    circ_orig, parameters_orig = utils.qasm_to_squander_circuit(filename)
     config['topology'] = [
     (0, 1), (0, 2), (0, 3), 
     ]
     wide_circuit_optimizer = Partition_Aware_Mapping( config )
-    circ, params, _,_ = wide_circuit_optimizer.Partition_Aware_Mapping( circ, parameters )
-    print(Qiskit_IO.get_Qiskit_Circuit(circ.get_Flat_Circuit(),params))
+    circ, params, input_perm,output_perm = wide_circuit_optimizer.Partition_Aware_Mapping( circ_orig, parameters_orig )
+    #print(Qiskit_IO.get_Qiskit_Circuit(circ.get_Flat_Circuit(),params))
+    num_qubits = circ.get_Qbit_Num() 
+    matrix_size = 1 << num_qubits 
+    initial_state_real = np.random.uniform(-1.0,1.0, (matrix_size,) )
+    initial_state_imag = np.random.uniform(-1.0,1.0, (matrix_size,) )
+    initial_state = initial_state_real + initial_state_imag*1j
+    initial_state = initial_state/np.linalg.norm(initial_state)
+    original_state = initial_state.copy()
+    circ_orig.apply_to(parameters_orig,original_state)
+    circ_Final = Circuit(circ.get_Qbit_Num() )
+    circ_Final.add_Permutation(input_perm)
+    circ_Final.add_Circuit(circ)
+    output_perm_T = [0]* circ.get_Qbit_Num() 
+    for i, j in enumerate(output_perm):
+        output_perm_T[j] = i
+    circ_Final.add_Permutation(output_perm)
+    PartAM_state = initial_state.copy()
+    circ_Final.apply_to(params,PartAM_state)
+    print(f"Decomposition error on random state: {1-abs(np.vdot(PartAM_state,original_state))}")
     print("--- %s seconds elapsed during optimization ---" % (time.time() - start_time))
 
 
