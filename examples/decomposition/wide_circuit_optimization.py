85c49160004206ac20eeadaa4f15e7c60040bd2d
Gregory Morse <gregory.morse@live.com>
2026-04-14 14:05:47 +0200

    Clean up unused code and streamline file handling
    Removed commented-out code related to downloading and processing QASM files, and updated file handling for optimization.

--- examples/decomposition/wide_circuit_optimization.py
+++ examples/decomposition/wide_circuit_optimization.py
@@ -45,64 +45,9 @@ if __name__ == "__main__":
         # **{'use_basin_hopping': True, 'bh_T': 1.1822334624366124, 'bh_stepsize': 0.9020671823381502, 'bh_interval': 165, 'bh_target_accept_rate': 0.7037812116166546, 'bh_stepwise_factor': 0.8254028860713254}
     }
 
-    #git clone https://github.com/onestruggler/qasm-quipper
-    #sudo yum install gmp-devel
-    #curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
-    #~/.ghcup/bin/ghcup install ghc 8.8.4 #Quipper supports up to GHC 8.8.4
-    #~/.ghcup/bin/ghcup set ghc 8.8.4
-    #~/.ghcup/bin/ghcup rm ghc 9.6.7
-    #PATH=~/.ghcup/bin:$PATH cabal update, cabal new-build
-    #~/qasm-quipper/dist-newstyle/build/x86_64-linux/ghc-8.8.4/LinguaQuanta-0.1.0.0/x/quip_to_qasm/build/quip_to_qasm/quip_to_qasm -s inp -o inp.qasm
-    #./scripts/quip_to_qasm2.sh -s inp -o inp.qasm
-    """
-    zip_url = "https://github.com/njross/optimizer/archive/refs/heads/master.zip"
-    #zip_url = "https://zenodo.org/records/17293975/files/benchmark_circuit_QMill_IBM.zip?download=1"
-    temp_dir = tempfile.mkdtemp(prefix="repos_")
-    zip_path = os.path.join(temp_dir, "master.zip")
-    qasm_files = []
-    # Download zip
-    r = requests.get(zip_url, stream=True)
-    if r.status_code != 200:
-        raise RuntimeError(f"Failed to download {zip_url}: HTTP {r.status_code}")
-    with open(zip_path, "wb") as f:
-        for chunk in r.iter_content(chunk_size=8192):
-            f.write(chunk)
+    import os
 
-    # Extract zip
-    extract_path = os.path.join(temp_dir, "master")
-    with zipfile.ZipFile(zip_path, "r") as zf:
-        zf.extractall(extract_path)
-
-    # Find QASM files
-    for path in Path(extract_path).rglob("*_before*"):
-        inp = str(path.resolve())
-        if "/PF/" in inp: continue
-        outp = inp + ".qasm"
-        #os.system(f"PATH=~/qasm-quipper/dist-newstyle/build/x86_64-linux/ghc-8.8.4/LinguaQuanta-0.1.0.0/x/elim_ctrls/build/elim_ctrls/:~/qasm-quipper/dist-newstyle/build/x86_64-linux/ghc-8.8.4/LinguaQuanta-0.1.0.0/x/elim_funs/build/elim_funs/:~/qasm-quipper/dist-newstyle/build/x86_64-linux/ghc-8.8.4/LinguaQuanta-0.1.0.0/x/elim_invs/build/elim_invs/:~/qasm-quipper/dist-newstyle/build/x86_64-linux/ghc-8.8.4/LinguaQuanta-0.1.0.0/x/elim_pows/build/elim_pows/:~/qasm-quipper/dist-newstyle/build/x86_64-linux/ghc-8.8.4/LinguaQuanta-0.1.0.0/x/format_qasm/build/format_qasm/:~/qasm-quipper/dist-newstyle/build/x86_64-linux/ghc-8.8.4/LinguaQuanta-0.1.0.0/x/quip_to_qasm/build/quip_to_qasm:$PATH ~/qasm-quipper/scripts/quip_to_qasm2.sh -s {inp} -o {outp}")
-        os.system(f"~/qasm-quipper/quipper-qasm/dist-newstyle/build/x86_64-linux/ghc-8.8.4/quipper-qasm-0.1.0.0/x/quipper-qasm/build/quipper-qasm/quipper-qasm -inline {inp} > {outp}")
-        cczdef = "gate ccz a,b,c { h c; ccx a,b,c; h c; }\n"
-        qasm_files.append(outp)
-        print(outp)
-    with zipfile.ZipFile("IBMeagle.zip", "w") as zf:
-        for path in Path(extract_path).rglob("*.qasm"):
-            zf.write(str(path.resolve()))    
-    """
-    #extract_dir = tempfile.mkdtemp(prefix="IBMeagle")
-    #with zipfile.ZipFile("IBMeagle.zip", "r") as zf:
-    #    zf.extractall(extract_dir)
-    #qasm_files = [str(path.resolve()) for path in Path(extract_dir).rglob("*.qasm")]
-    #filename = next(x for x in qasm_files if "mod5_4_before" in x)
-    #filename = next(x for x in qasm_files if "gf2^E8_mult_before" in x)
-    #filename = next(x for x in qasm_files if "csum_mux_9_before" in x)
-
-    import glob, re, os
-    files = list(sorted((filename for filename in glob.glob("/home/morse/processed_qasm/**/*.qasm", recursive=True) if "_u3cx" not in filename),
-        key=lambda x: re.sub(r'(\d+)', lambda m: f"{int(m.group(1)):010}", x)))
-
-
-    #import os
-
-    #files = [os.path.join(Path(__file__).resolve().parent, "bv_n14.qasm")]
+    files = [os.path.join(Path(__file__).resolve().parent, "bv_n14.qasm")]
 
     results = {}
     for filename in files:
